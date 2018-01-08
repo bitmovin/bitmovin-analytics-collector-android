aab6c64ab6a64b4344f5b1aee31d42f6ca58e9fe
Cory Zachman <cory.zachman@bitmovin.com>
2018-01-07 20:03:54 -0700

    changed to queue, but not adding to it. Analytics backend server does not support it

--- bitmovinanalyticscollector/src/main/java/com/bitmovin/bitmovinanalyticscollector/data/IEventDataDispatcher.java
+++ bitmovinanalyticscollector/src/main/java/com/bitmovin/bitmovinanalyticscollector/data/IEventDataDispatcher.java
@@ -5,8 +5,8 @@ package com.bitmovin.bitmovinanalyticscollector.data;
  */
 
 public interface IEventDataDispatcher {
-
-
+    public void enable();
+    public void disable();
     public void add(EventData data);
     public void clear();
 }
--- bitmovinanalyticscollector/src/main/java/com/bitmovin/bitmovinanalyticscollector/data/SimpleEventDataDispatcher.java
+++ bitmovinanalyticscollector/src/main/java/com/bitmovin/bitmovinanalyticscollector/data/SimpleEventDataDispatcher.java
@@ -9,7 +9,8 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 import java.util.List;
-import java.util.Stack;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by zachmanc on 12/29/17.
@@ -18,21 +19,31 @@ import java.util.Stack;
 public class SimpleEventDataDispatcher implements IEventDataDispatcher {
     private static final String TAG = "SimpleDispatcher";
 
-    private Stack<EventData> data;
+    private Queue<EventData> data;
     private HttpClient httpClient;
+    private boolean enabled;
 
     public SimpleEventDataDispatcher(BitmovinAnalyticsConfig config) {
-        this.data = new Stack<EventData>();
+        this.data = new ConcurrentLinkedQueue<EventData>();
         this.httpClient = new HttpClient(config);
     }
 
     @Override
+    public void enable() {
+        this.enabled = true;
+    }
+
+    @Override
+    public void disable() {
+        this.enabled = false;
+    }
+
+    @Override
     public void add(EventData data) {
-        // Currently not using the stack and just sending the data as it comes in. Not sure the format to send multiple messages at a time
-        this.data.push(data);
+//      Currently not using the stack and just sending the data as it comes in. Not sure the format to send multiple messages at a time
+//      this.data.add(data);
         Gson gson = new Gson();
         String json = gson.toJson(data);
-        Log.d(TAG,json);
 
         this.httpClient.post(json);
     }
@@ -46,11 +57,13 @@ public class SimpleEventDataDispatcher implements IEventDataDispatcher {
         ArrayList<EventData> list = new ArrayList<EventData>();
         for (int i=0; i<count; i++){
             try{
-                list.add(data.pop());
+                list.add(data.remove());
             }catch (EmptyStackException e){
                 break;
             }
         }
         return list;
     }
+
+
 }
