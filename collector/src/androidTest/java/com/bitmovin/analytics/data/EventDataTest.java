0dcdf6f95a56dd1e71f270679e5638401331274c
Daniel Hoelbling-Inzko <daniel.hoelbling-inzko@bitmovin.com>
2019-09-04 09:30:38 +0200

    Fixed and added better tests

--- collector/src/androidTest/java/com/bitmovin/analytics/data/EventDataTest.java
+++ collector/src/androidTest/java/com/bitmovin/analytics/data/EventDataTest.java
@@ -1,6 +1,7 @@
 package com.bitmovin.analytics.data;
 
 import android.content.Context;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -32,17 +33,6 @@ public class EventDataTest {
         eventData = new EventData(bitmovinAnalyticsConfig, appContext,"1234", "user agent");
     }
 
-    @Test
-    public void serializeTest() throws Exception {
-        String expected = "{\"ad\":0,\"analyticsVersion\":\"0.1\",\"audioBitrate\":0,\"buffered\":0,\"cdnProvider\":\"akamai\",\"domain\":\"com.bitmovin.bitmovinanalyticscollector.test\",\"droppedFrames\":0,\"duration\":0,\"impressionId\":\"1234\",\"isCasting\":false,\"isLive\":false,\"isMuted\":false,\"key\":\"9ae0b480-f2ee-4c10-bc3c-cb88e982e0ac\",\"language\":\"en_US\",\"pageLoadTime\":0,\"pageLoadType\":0,\"path\":\"\",\"paused\":0,\"played\":0,\"playerKey\":\"18ca6ad5-9768-4129-bdf6-17685e0d14d2\",\"playerStartupTime\":0,\"playerTech\":\"Native\",\"screenHeight\":2712,\"screenWidth\":1440,\"seeked\":0,\"startupTime\":0,\"time\":0,\"userAgent\":\"Unknown/null (Linux;Android 8.1.0) ExoPlayerLib/2.6.1\",\"userId\":\"0e9717098feb0d7a\",\"videoBitrate\":0,\"videoDuration\":0,\"videoId\":\"video1234\",\"videoPlaybackHeight\":0,\"videoPlaybackWidth\":0,\"videoStartupTime\":0,\"videoTimeEnd\":0,\"videoTimeStart\":0,\"videoWindowHeight\":0,\"videoWindowWidth\":0}";
-        Gson gson = new Gson();
-        String json = gson.toJson(eventData);
-
-        System.out.println(expected);
-        System.out.println(json);
-        Assert.assertEquals(json, expected);
-    }
-
     // Test isn't running because I can't get android to compile the apk for the tests :/
     @Test
     public void eventDataContainsDeviceInformation() throws Exception {
@@ -51,5 +41,7 @@ public class EventDataTest {
         EventData data = new EventData(config, appContext, "1234", "user agent");
         String serialized = DataSerializer.serialize(data);
         assertThat(serialized).contains("\"deviceInformation\":{");
+        assertThat(serialized).contains(String.format("\"model\":\"%s\"", Build.MODEL));
+        assertThat(serialized).contains(String.format("\"manufacturer\":\"%s\"", Build.MANUFACTURER));
     }
 }
