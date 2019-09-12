4eeb718b08569eb9dde0270159bdd4827ab5f9ad
Roland Griesser <roland.griesser@bitmovin.com>
2019-09-12 16:20:09 +0200

    Added  moduleInformation

--- collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/BitmovinSdkAdAdapter.kt
+++ collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/BitmovinSdkAdAdapter.kt
@@ -2,14 +2,16 @@ package com.bitmovin.analytics.bitmovin.player
 
 import com.bitmovin.analytics.BitmovinAdAnalytics
 import com.bitmovin.analytics.adapters.AdAdapter
+import com.bitmovin.analytics.data.AdModuleInformation
 import com.bitmovin.player.BitmovinPlayer
 
 class BitmovinSdkAdAdapter(val bitmovinPlayer: BitmovinPlayer, val adAnalytics: BitmovinAdAnalytics) : AdAdapter {
-    init {
-
-    }
 
     override fun release() {
 
     }
+
+    override val moduleInformation: AdModuleInformation
+        // TODO get actual module from player
+        get() = AdModuleInformation("DefaultAdvertisingService", BitmovinUtil.getPlayerVersion())
 }
