2f92cfee46726a9a36c51dedead7f07ee19682bf
Roland Griesser <roland.griesser@bitmovin.com>
2019-09-12 11:18:18 +0200

    Added sendAd method

--- collector/src/main/java/com/bitmovin/analytics/data/HttpBackend.java
+++ collector/src/main/java/com/bitmovin/analytics/data/HttpBackend.java
@@ -11,11 +11,14 @@ import com.bitmovin.analytics.utils.HttpClient;
 public class HttpBackend implements Backend {
     private static final String TAG = "BitmovinBackend";
     private HttpClient httpClient;
+    private String analyticsBackendUrl;
+    private String adsAnalyticsBackendUrl;
 
     public HttpBackend(CollectorConfig config, Context context) {
-        String backendUrl = Uri.parse(config.getBackendUrl()).buildUpon().appendEncodedPath("analytics").build().toString();
-        Log.d(TAG, String.format("Initialized Analytics HTTP Backend with %s", backendUrl));
-        this.httpClient = new HttpClient(context, backendUrl);
+        analyticsBackendUrl = Uri.parse(config.getBackendUrl()).buildUpon().appendEncodedPath("analytics").build().toString();
+        adsAnalyticsBackendUrl = Uri.parse(config.getBackendUrl()).buildUpon().appendEncodedPath("analytics/a").build().toString();
+        Log.d(TAG, String.format("Initialized Analytics HTTP Backend with %s", analyticsBackendUrl));
+        this.httpClient = new HttpClient(context);
     }
 
     @Override
@@ -27,6 +30,18 @@ public class HttpBackend implements Backend {
                 eventData.getStartupTime(),
                 eventData.getVideoStartupTime(),
                 eventData.getBuffered()));
-        this.httpClient.post(DataSerializer.serialize(eventData), null);
+        this.httpClient.post(analyticsBackendUrl, DataSerializer.serialize(eventData), null);
+    }
+
+    @Override
+    public void sendAd(AdEventData eventData) {
+//        Log.d(TAG, String.format("Sending ad sample: %s (state: %s, videoId: %s, startupTime: %d, videoStartupTime: %d, buffered: %d)",
+//                eventData.getImpressionId(),
+//                eventData.getVideoId(),
+//                eventData.getState(),
+//                eventData.getStartupTime(),
+//                eventData.getVideoStartupTime(),
+//                eventData.getBuffered()));
+        this.httpClient.post(adsAnalyticsBackendUrl, DataSerializer.serialize(eventData), null);
     }
 }
