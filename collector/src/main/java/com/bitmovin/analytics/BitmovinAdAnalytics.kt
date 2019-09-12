ccd20d9c82b0be053651e7c2d27ec59eec7475bd
Roland Griesser <roland.griesser@bitmovin.com>
2019-09-12 14:17:04 +0200

    Added eventhandlers

--- collector/src/main/java/com/bitmovin/analytics/BitmovinAdAnalytics.kt
+++ collector/src/main/java/com/bitmovin/analytics/BitmovinAdAnalytics.kt
@@ -1,5 +1,47 @@
 package com.bitmovin.analytics
 
 class BitmovinAdAnalytics(var analytics: BitmovinAnalytics) {
-    
+    fun onAdStarted() {
+
+    }
+
+    fun onAdFinished() {
+
+    }
+
+    fun onAdBreakStarted() {
+
+    }
+
+    fun onAdBreakFinished() {
+
+    }
+
+    fun onAdClicked() {
+
+    }
+
+    fun onAdError() {
+
+    }
+
+    fun onAdManifestLoaded() {
+
+    }
+
+    fun onPlay() {
+
+    }
+
+    fun onPause() {
+
+    }
+
+    fun onAdSkipped() {
+
+    }
+
+    fun onAdQuartile() {
+
+    }
 }
