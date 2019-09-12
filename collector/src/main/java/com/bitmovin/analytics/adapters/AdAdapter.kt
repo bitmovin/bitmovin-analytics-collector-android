322eec78fc659b85593b6648c3e8cb0fcd491e11
Roland Griesser <roland.griesser@bitmovin.com>
2019-09-12 14:16:34 +0200

    Added AdModuleInformation

--- collector/src/main/java/com/bitmovin/analytics/adapters/AdAdapter.kt
+++ collector/src/main/java/com/bitmovin/analytics/adapters/AdAdapter.kt
@@ -1,5 +1,8 @@
 package com.bitmovin.analytics.adapters
 
+import com.bitmovin.analytics.data.AdModuleInformation
+
 interface AdAdapter {
     fun release()
+    val moduleInformation: AdModuleInformation
 }
---
+++ collector/src/main/java/com/bitmovin/analytics/data/AdModuleInformation.kt
@@ -0,0 +1,3 @@
+package com.bitmovin.analytics.data
+
+data class AdModuleInformation(var name: String, var version: String)
