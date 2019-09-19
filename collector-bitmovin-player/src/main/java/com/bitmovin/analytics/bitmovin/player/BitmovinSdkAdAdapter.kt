9f898abb9b8e4c529d990f06437b71d41f6a151a
Roland Griesser <roland.griesser@bitmovin.com>
2019-09-19 08:19:09 +0200

    Added event listeners to player

--- collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/BitmovinSdkAdAdapter.kt
+++ collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/BitmovinSdkAdAdapter.kt
@@ -1,11 +1,74 @@
 package com.bitmovin.analytics.bitmovin.player
 
+import android.util.Log
 import com.bitmovin.analytics.BitmovinAdAnalytics
 import com.bitmovin.analytics.adapters.AdAdapter
 import com.bitmovin.analytics.data.AdModuleInformation
+import com.bitmovin.analytics.stateMachines.PlayerState
 import com.bitmovin.player.BitmovinPlayer
+import com.bitmovin.player.api.event.data.AdStartedEvent
+import com.bitmovin.player.api.event.data.StallStartedEvent
+import com.bitmovin.player.api.event.listener.*
 
 class BitmovinSdkAdAdapter(val bitmovinPlayer: BitmovinPlayer, val adAnalytics: BitmovinAdAnalytics) : AdAdapter {
+
+    private val onAdStartedListener = OnAdStartedListener {
+        adAnalytics.onAdStarted()
+    }
+
+    private val onAdFinishedListener = OnAdFinishedListener {
+        adAnalytics.onAdFinished()
+    }
+
+    private val onAdBreakStartedListener = OnAdBreakStartedListener {
+        adAnalytics.onAdBreakStarted()
+    }
+
+    private val onAdBreakFinishedListener = OnAdBreakFinishedListener {
+        adAnalytics.onAdBreakFinished()
+    }
+
+    private val onAdClickedListener = OnAdClickedListener {
+        adAnalytics.onAdClicked(it.clickThroughUrl)
+    }
+
+    private val onAdErrorListener = OnAdErrorListener {
+        adAnalytics.onAdError(null, it.code, it.message)
+    }
+
+    private val onAdSkippedListener = OnAdSkippedListener {
+        adAnalytics.onAdSkipped()
+    }
+
+    private val onAdManifestLoadedListener = OnAdManifestLoadedListener {
+        adAnalytics.onAdManifestLoaded()
+    }
+
+    private val onPlayListener = OnPlayListener {
+        adAnalytics.onPlay()
+    }
+
+    private val onPausedListener = OnPausedListener {
+        adAnalytics.onPause()
+    }
+
+    private val onAdQuartileListener = OnAdQuartileListener {
+        adAnalytics.onAdQuartile()
+    }
+
+    init {
+        bitmovinPlayer.addEventListener(onAdStartedListener)
+        bitmovinPlayer.addEventListener(onAdFinishedListener)
+        bitmovinPlayer.addEventListener(onAdBreakStartedListener)
+        bitmovinPlayer.addEventListener(onAdBreakFinishedListener)
+        bitmovinPlayer.addEventListener(onAdClickedListener)
+        bitmovinPlayer.addEventListener(onAdErrorListener)
+        bitmovinPlayer.addEventListener(onAdSkippedListener)
+        bitmovinPlayer.addEventListener(onAdManifestLoadedListener)
+        bitmovinPlayer.addEventListener(onPlayListener)
+        bitmovinPlayer.addEventListener(onPausedListener)
+        bitmovinPlayer.addEventListener(onAdQuartileListener)
+    }
 
     override fun release() {
 
