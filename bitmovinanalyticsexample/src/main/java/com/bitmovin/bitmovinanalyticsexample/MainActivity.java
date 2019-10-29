f69cd24b6c948a6d58a089af7d0e0934856d1c57
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2019-10-29 10:01:07 +0100

    correct Ad Player setup for Example App

--- bitmovinanalyticsexample/src/main/java/com/bitmovin/bitmovinanalyticsexample/MainActivity.java
+++ bitmovinanalyticsexample/src/main/java/com/bitmovin/bitmovinanalyticsexample/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.bitmovin.analytics.BitmovinAnalytics;
 import com.bitmovin.analytics.BitmovinAnalyticsConfig;
 import com.bitmovin.analytics.bitmovin.player.BitmovinPlayerCollector;
 import com.bitmovin.analytics.enums.CDNProvider;
@@ -13,6 +14,10 @@ import com.bitmovin.player.BitmovinPlayerView;
 import com.bitmovin.player.config.PlaybackConfiguration;
 import com.bitmovin.player.config.PlayerConfiguration;
 import com.bitmovin.player.config.media.SourceConfiguration;
+import com.bitmovin.player.config.advertising.AdItem;
+import com.bitmovin.player.config.advertising.AdSource;
+import com.bitmovin.player.config.advertising.AdSourceType;
+import com.bitmovin.player.config.advertising.AdvertisingConfiguration;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -35,21 +40,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         changeSource.setOnClickListener(this);
 
         this.bitmovinPlayerView = this.findViewById(R.id.bitmovinPlayerView);
-        this.bitmovinPlayer = this.bitmovinPlayerView.getPlayer();
-        PlaybackConfiguration playbackConfiguration = this.bitmovinPlayer.getConfig().getPlaybackConfiguration();
+
+        PlayerConfiguration config = new PlayerConfiguration();
+
+        SourceConfiguration source = this.createSource();
+        config.setSourceConfiguration(source);
+
+        AdvertisingConfiguration adConfig = initializeAds(config);
+        config.setAdvertisingConfiguration(adConfig);
+
+        PlaybackConfiguration playbackConfiguration = config.getPlaybackConfiguration();
         playbackConfiguration.setMuted(true);
-        playbackConfiguration.setAutoplayEnabled(true);
+        playbackConfiguration.setAutoplayEnabled(false);
 
-        PlayerConfiguration config = this.bitmovinPlayer.getConfig();
-        this.initializeAnalytics();
+        this.bitmovinAnalytics = this.initializeAnalytics();
 
-        this.initializePlayer();
+        this.bitmovinPlayer = new BitmovinPlayer(getApplicationContext(), config);
+        this.bitmovinAnalytics.attachPlayer(bitmovinPlayer);
+
+        this.bitmovinPlayerView.setPlayer(this.bitmovinPlayer);
+
 
     }
 
-    protected void initializeAnalytics() {
+    protected BitmovinPlayerCollector initializeAnalytics() {
         //Step 1: Create your analytics config object
-        BitmovinAnalyticsConfig bitmovinAnalyticsConfig = new BitmovinAnalyticsConfig("<YOUR_ANALYTICS_KEY>", "<YOUR_PLAYER_KEY>", getApplicationContext());
+        BitmovinAnalyticsConfig bitmovinAnalyticsConfig = new BitmovinAnalyticsConfig("17e6ea02-cb5a-407f-9d6b-9400358fbcc0");
 
         //Step 2: Add optional parameters
         bitmovinAnalyticsConfig.setVideoId("androidVideoDASHStatic");
@@ -64,24 +80,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bitmovinAnalyticsConfig.setCustomData5("customData5");
         bitmovinAnalyticsConfig.setPath("/vod/new/");
         bitmovinAnalyticsConfig.setHeartbeatInterval(59700);
+        bitmovinAnalyticsConfig.setAds(true);
 
         //Step 3: Create Analytics Collector
-        bitmovinAnalytics = new BitmovinPlayerCollector(bitmovinAnalyticsConfig);
+        return new BitmovinPlayerCollector(bitmovinAnalyticsConfig, getApplicationContext());
     }
 
 
-    protected void initializePlayer() {
+    protected SourceConfiguration createSource() {
         // Create a new source configuration
         SourceConfiguration sourceConfiguration = new SourceConfiguration();
 
         // Add a new source item
         sourceConfiguration.addSourceItem("https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd");
 
-        //Step 4: Attach BitmovinPlayer
-        bitmovinAnalytics.attachPlayer(bitmovinPlayer);
-
-        // load source using the created source configuration
-        bitmovinPlayer.load(sourceConfiguration);
+        return sourceConfiguration;
     }
 
     @Override
@@ -114,7 +127,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == releaseButton) {
             releasePlayer();
         } else if (v == createButton) {
-            initializePlayer();
+            createSource();
         } else if (v == changeSource) {
             onPlayerChangeSource();
         }
@@ -124,5 +137,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         SourceConfiguration config = new SourceConfiguration();
         config.addSourceItem("http://bitdash-a.akamaihd.net/content/sintel/sintel.mpd");
         bitmovinPlayer.load(config);
+    }
+    private static final String AD_SOURCE_1 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirecterror&nofb=1&correlator=";
+    private static final String AD_SOURCE_2 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=";
+    private static final String AD_SOURCE_3 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=";
+    private static final String AD_SOURCE_4 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirectlinear&correlator=";
+
+
+    private AdvertisingConfiguration initializeAds(PlayerConfiguration config) {
+        // Create AdSources
+        AdSource firstAdSource = new AdSource(AdSourceType.IMA, AD_SOURCE_1);
+        AdSource secondAdSource = new AdSource(AdSourceType.IMA, AD_SOURCE_2);
+        AdSource thirdAdSource = new AdSource(AdSourceType.IMA, AD_SOURCE_3);
+        AdSource fourthAdSource = new AdSource(AdSourceType.IMA, AD_SOURCE_4);
+
+        // Setup a pre-roll ad
+        AdItem preRoll = new AdItem("pre", thirdAdSource);
+        // Setup a mid-roll waterfalling ad at 10% of the content duration
+        // NOTE: AdItems containing more than one AdSource, will be executed as waterfalling ad
+        AdItem midRoll = new AdItem("10%", firstAdSource, secondAdSource);
+        // Setup a post-roll ad
+        AdItem postRoll = new AdItem("post", fourthAdSource);
+
+        // Add the AdItems to the AdvertisingConfiguration
+        AdvertisingConfiguration advertisingConfiguration = new AdvertisingConfiguration(preRoll, midRoll, postRoll);
+        return advertisingConfiguration;
     }
 }
