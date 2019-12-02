74b60c56ea9668b9c8fa274e6732a8711985d465
Peter Pfaffeneder <peter.pfaffeneder@bitmovin.com>
2019-12-02 17:49:01 +0100

    Removed unused file

--- collector-exoplayer/src/main/java/com/bitmovin/analytics/exoplayer/ExoPlayerModule.kt
+++
@@ -1,7 +0,0 @@
-package com.bitmovin.analytics.exoplayer
-
-enum class ExoPlayerModule(val moduleName: String) {
-    HLS("goog.exo.hls"),
-    DASH("goog.exo.dash"),
-    SMOOTH_STREAMING("goog.exo.smoothstreaming")
-}
