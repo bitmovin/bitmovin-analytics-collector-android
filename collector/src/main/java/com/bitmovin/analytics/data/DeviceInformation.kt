d54e58ba05ced31455dd26e42558205c2e3962fd
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2020-04-30 15:39:42 +0200

    intendation fix

--- collector/src/main/java/com/bitmovin/analytics/data/DeviceInformation.kt
+++ collector/src/main/java/com/bitmovin/analytics/data/DeviceInformation.kt
@@ -1,12 +1,12 @@
 package com.bitmovin.analytics.data
 
 data class DeviceInformation(
-        val manufacturer: String,
-        val model: String,
-        val isTV: Boolean,
-        val userAgent: String,
-        val locale: String,
-        val packageName: String,
-        val screenHeight: Int,
-        val screenWidth: Int
+    val manufacturer: String,
+    val model: String,
+    val isTV: Boolean,
+    val userAgent: String,
+    val locale: String,
+    val packageName: String,
+    val screenHeight: Int,
+    val screenWidth: Int
 )
