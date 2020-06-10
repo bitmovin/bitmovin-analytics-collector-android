736f6d47681c29154b36fc6fc49cca23666a2b4b
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2020-06-10 14:09:36 +0200

    finished test for quality change behaviour

--- collector-exoplayer/src/test/java/com/bitmovin/analytics/exoplayer/ExoPlayerAdapterTest.kt
+++ collector-exoplayer/src/test/java/com/bitmovin/analytics/exoplayer/ExoPlayerAdapterTest.kt
@@ -1,7 +1,7 @@
 package com.bitmovin.analytics.exoplayer
 
-import android.content.Context
 import com.bitmovin.analytics.BitmovinAnalyticsConfig
+import com.bitmovin.analytics.data.EventDataFactory
 import com.bitmovin.analytics.stateMachines.PlayerState
 import com.bitmovin.analytics.stateMachines.PlayerStateMachine
 import com.google.android.exoplayer2.ExoPlayer
@@ -11,34 +11,36 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
-import org.mockito.Mockito.any
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
-import org.mockito.Mockito.verifyZeroInteractions
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.verification.VerificationMode
 
 @RunWith(MockitoJUnitRunner::class)
 class ExoPlayerAdapterTest {
 
-    private lateinit var adapter: ExoPlayerAdapter
+
+    private lateinit var adapter: FakeExoPlayerAdapter
     @Mock
     private lateinit var stateMachine: PlayerStateMachine
     @Before
     fun setup(){
-        adapter = ExoPlayerAdapter(mock(ExoPlayer::class.java), mock(BitmovinAnalyticsConfig::class.java), mock(Context::class.java), stateMachine)
+        adapter = FakeExoPlayerAdapter(mock(ExoPlayer::class.java), mock(BitmovinAnalyticsConfig::class.java), mock(EventDataFactory::class.java), stateMachine)
     }
 
+
+
     @Test
-    fun test(){
+    fun testNoStateTransitionToQualityChangeIfBitrateDidNotChange(){
         val bitrate = 3000
         `when`(stateMachine.currentState).thenReturn(PlayerState.PLAYING)
+        adapter.fakePosition = 20
         adapter.onDecoderInputFormatChanged(mock(AnalyticsListener.EventTime::class.java),0,  Format.createVideoSampleFormat(null, null, null,  bitrate, 1,  300, 300, 64F, null, null))
-        verify(stateMachine).transitionState(PlayerState.QUALITYCHANGE, ArgumentMatchers.anyLong())
+        verify(stateMachine, times(1)).transitionState(eq(PlayerState.QUALITYCHANGE), ArgumentMatchers.anyLong())
         adapter.onDecoderInputFormatChanged(mock(AnalyticsListener.EventTime::class.java),0,  Format.createVideoSampleFormat(null, null, null,  bitrate, 1,  300, 300, 64F, null, null))
-        verifyZeroInteractions(stateMachine)
+        verify(stateMachine, times(1)).transitionState(eq(PlayerState.QUALITYCHANGE), ArgumentMatchers.anyLong())
     }
 }
