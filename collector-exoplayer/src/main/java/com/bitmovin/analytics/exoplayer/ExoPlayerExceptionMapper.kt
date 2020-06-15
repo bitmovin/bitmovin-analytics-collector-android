5414bccb8e57bc5cb3628d848033aaeeddd024a6
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2020-06-15 16:31:33 +0200

    refactored error code mapping in ExoPlayer

--- collector-exoplayer/src/main/java/com/bitmovin/analytics/exoplayer/ExoPlayerExceptionMapper.kt
+++ collector-exoplayer/src/main/java/com/bitmovin/analytics/exoplayer/ExoPlayerExceptionMapper.kt
@@ -1,69 +1,63 @@
 package com.bitmovin.analytics.exoplayer
 
-import com.bitmovin.analytics.data.ErrorCode
 import com.bitmovin.analytics.data.ErrorData
+import com.bitmovin.analytics.data.ErrorCode
 import com.bitmovin.analytics.error.ExceptionMapper
 import com.bitmovin.analytics.utils.topOfStacktrace
 import com.google.android.exoplayer2.ExoPlaybackException
-import com.google.android.exoplayer2.ExoPlaybackException.TYPE_RENDERER
-import com.google.android.exoplayer2.ExoPlaybackException.TYPE_SOURCE
-import com.google.android.exoplayer2.ExoPlaybackException.TYPE_UNEXPECTED
 import com.google.android.exoplayer2.source.BehindLiveWindowException
 import com.google.android.exoplayer2.upstream.HttpDataSource
 
 class ExoPlayerExceptionMapper : ExceptionMapper<Throwable> {
-    private fun doMap(error: ExoPlaybackException): ErrorCode {
-        var errorCode = ErrorCode.UNKNOWN_ERROR
-        when (error.type) {
-            TYPE_SOURCE -> {
-                val exception = error.sourceException
-                if (exception is HttpDataSource.InvalidResponseCodeException) {
-                    errorCode = ErrorCode.DATASOURCE_HTTP_FAILURE
-                    errorCode.errorData = ErrorData("Data Source request failed with HTTP status: " + exception.responseCode + " - " + exception.dataSpec.uri)
-                } else if (exception is HttpDataSource.InvalidContentTypeException) {
-                    errorCode = ErrorCode.DATASOURCE_INVALID_CONTENT_TYPE
-                    errorCode.errorData = ErrorData("Invalid Content Type: " + exception.contentType)
-                } else if (exception is HttpDataSource.HttpDataSourceException) {
-                    errorCode = ErrorCode.DATASOURCE_UNABLE_TO_CONNECT
-                    errorCode.errorData = ErrorData("Unable to connect: " + exception.dataSpec.uri)
-                } else if (exception is BehindLiveWindowException) {
-                    errorCode = ErrorCode.BEHIND_LIVE_WINDOW
-                    errorCode.errorData = ErrorData("Behind live window: required segments not available")
-                }
 
-                if (errorCode.errorData == null) {
-                    errorCode.errorData = ErrorData(exception::class.java.toString() + ": " + (exception.message
-                            ?: ""), exception.topOfStacktrace)
-                }
-            }
-            TYPE_RENDERER -> {
-                errorCode = ErrorCode.EXOPLAYER_RENDERER_ERROR
-                errorCode.errorData = ErrorData(error.rendererException.message
-                        ?: "", error.rendererException.topOfStacktrace)
-            }
-            TYPE_UNEXPECTED -> {
-                errorCode = ErrorCode.UNKNOWN_ERROR
-                errorCode.errorData = ErrorData(error.unexpectedException.message
-                        ?: "", error.unexpectedException.topOfStacktrace)
-            }
-            else -> {
-                errorCode = ErrorCode.UNKNOWN_ERROR
-                errorCode.errorData = ErrorData(error.message
-                        ?: "", error.topOfStacktrace)
-            }
-        }
-        return errorCode
-    }
+    private val errorMessages = mapOf(
+            -1 to "Unknown Error",
+            0 to "Source Error",
+            1 to "Render Error",
+            2 to "Unexpected Error",
+            3 to "Remote Error",
+            4 to "Out of memory Error")
 
     override fun map(throwable: Throwable): ErrorCode {
-        return when (throwable) {
-            is ExoPlaybackException -> doMap(throwable)
-            else -> {
-                val errorCode = ErrorCode.UNKNOWN_ERROR
-                errorCode.errorData = ErrorData(throwable.message
-                        ?: "", throwable.topOfStacktrace)
-                return errorCode
+
+        val type = getExceptionType(throwable)
+        var message = errorMessages[type] ?: "Unknown Error"
+        var errorData: ErrorData? = null
+
+        val cause = throwable.cause
+        if (cause != null) {
+            when (cause) {
+                is HttpDataSource.InvalidResponseCodeException -> {
+                    message += ": InvalidResponseCodeException"
+                    errorData = ErrorData("Data Source request failed with HTTP status: " + cause.responseCode + " - " + cause.dataSpec.uri, cause.topOfStacktrace)
+                }
+                is HttpDataSource.InvalidContentTypeException -> {
+                    message += ": InvalidContentTypeException"
+                    errorData = ErrorData("Invalid Content Type: " + cause.contentType, cause.topOfStacktrace)
+                }
+                is HttpDataSource.HttpDataSourceException -> {
+                    message += ": HttpDataSourceException"
+                    errorData = ErrorData("Unable to connect: " + cause.dataSpec.uri, cause.topOfStacktrace)
+                }
+                is BehindLiveWindowException -> {
+                    message += ": BehindLiveWindowException"
+                    errorData = ErrorData("Behind live window: required segments not available", cause.topOfStacktrace)
+                }
+                else -> errorData = null
             }
+            if (errorData == null) {
+                errorData = ErrorData(cause.message
+                        ?: "", cause.topOfStacktrace)
+            }
+        }
+
+        return ErrorCode(type, message, errorData)
+    }
+
+    private fun getExceptionType(throwable: Throwable): Int {
+        return when (throwable) {
+            is ExoPlaybackException -> throwable.type
+            else -> -1
         }
     }
 }
