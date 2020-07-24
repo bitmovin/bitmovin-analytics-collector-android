2dd0c06c7ea95cbcbbd7fb4e52cb0e7a3e4c0285
Ines Kulenovic <ines.kulenovic@bitmovin.com>
2020-07-24 14:26:25 +0200

    Format fix

--- collector/src/main/java/com/bitmovin/analytics/enums/AnalyticsErrorCodes.kt
+++ collector/src/main/java/com/bitmovin/analytics/enums/AnalyticsErrorCodes.kt
@@ -3,6 +3,6 @@ package com.bitmovin.analytics.enums
 import com.bitmovin.analytics.data.ErrorCode
 
 enum class AnalyticsErrorCodes(val errorCode: ErrorCode) {
-    ANALYTICS_QUALITY_CHANGE_THRESHOLD_EXCEEDED( ErrorCode(10000, "ANALYTICS_QUALITY_CHANGE_THRESHOLD_EXCEEDED", null)),
+    ANALYTICS_QUALITY_CHANGE_THRESHOLD_EXCEEDED(ErrorCode(10000, "ANALYTICS_QUALITY_CHANGE_THRESHOLD_EXCEEDED", null)),
     ANALYTICS_BUFFERING_TIMEOUT_REACHED(ErrorCode(10001, "ANALYTICS_BUFFERING_TIMEOUT_REACHED", null))
 }
