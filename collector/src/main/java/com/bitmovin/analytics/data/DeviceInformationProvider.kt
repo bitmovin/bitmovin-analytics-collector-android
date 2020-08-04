03cd97ff418ed8723a5d11be2c23e4128af8078e
Ines Kulenovic <ines.kulenovic@bitmovin.com>
2020-08-04 09:07:57 +0200

    Getting device width and height from resources.configuration

--- collector/src/main/java/com/bitmovin/analytics/data/DeviceInformationProvider.kt
+++ collector/src/main/java/com/bitmovin/analytics/data/DeviceInformationProvider.kt
@@ -3,11 +3,26 @@ package com.bitmovin.analytics.data
 import android.app.UiModeManager
 import android.content.Context
 import android.content.res.Configuration
+
 import android.os.Build
+import android.util.DisplayMetrics
+import android.view.WindowManager
 import com.bitmovin.analytics.utils.Util
+import kotlin.math.roundToInt
+
 
 open class DeviceInformationProvider(val context: Context, val userAgent: String) {
-    var isTV: Boolean = isTVDevice()
+   var isTV: Boolean = isTVDevice()
+
+    private val windowManager: WindowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    private val displayMetrics = DisplayMetrics()
+
+    val res = windowManager.defaultDisplay.getMetrics(displayMetrics)
+    val configuration: Configuration = context.resources.configuration
+
+    val width = (configuration.screenWidthDp * displayMetrics.density).roundToInt()
+    val height = (configuration.screenHeightDp * displayMetrics.density).roundToInt()
+
 
     fun getDeviceInformation(): DeviceInformation {
         return DeviceInformation(
@@ -16,8 +31,8 @@ open class DeviceInformationProvider(val context: Context, val userAgent: String
                 isTV = isTV,
                 locale = Util.getLocale(),
                 packageName = context.packageName,
-                screenWidth = context?.getResources()?.getDisplayMetrics()?.widthPixels ?: 0,
-                screenHeight = context?.getResources()?.getDisplayMetrics()?.heightPixels ?: 0,
+                screenWidth = width,
+                screenHeight = height,
                 userAgent = userAgent
         )
     }
