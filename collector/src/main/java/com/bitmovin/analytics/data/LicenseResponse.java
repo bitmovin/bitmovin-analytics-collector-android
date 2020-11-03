267ed9e3e2214ca5bd1d662b10a44041d10c5b23
Roland Griesser <roland.griesser@bitmovin.com>
2020-11-03 17:32:10 +0100

    Added settings

--- collector/src/main/java/com/bitmovin/analytics/data/LicenseResponse.java
+++ collector/src/main/java/com/bitmovin/analytics/data/LicenseResponse.java
@@ -1,8 +1,11 @@
 package com.bitmovin.analytics.data;
 
+import java.util.Map;
+
 public class LicenseResponse {
     private String status;
     private String message;
+    private Map<String, String> settings;
 
     public String getStatus() {
         return status;
@@ -18,5 +21,15 @@ public class LicenseResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Map<String, String> getSettings()
+    {
+        return settings;
+    }
+
+    public void setSettings(Map<String, String> settings)
+    {
+        this.settings = settings;
     }
 }
