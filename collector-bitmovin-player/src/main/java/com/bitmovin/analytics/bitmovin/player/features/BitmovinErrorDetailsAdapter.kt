97c5096c73f68530da47f60e91769caa96e10dfa
Roland Griesser <roland.griesser@bitmovin.com>
2020-11-05 15:57:15 +0100

    Implemented EventSource

--- collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/features/BitmovinErrorDetailsAdapter.kt
+++ collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/features/BitmovinErrorDetailsAdapter.kt
@@ -1,11 +1,15 @@
 package com.bitmovin.analytics.bitmovin.player.features
 
+import com.bitmovin.analytics.features.EventEmitter
+import com.bitmovin.analytics.features.EventSource
+import com.bitmovin.analytics.features.errordetails.ErrorDetailsEventListener
 import com.bitmovin.player.BitmovinPlayer
 import com.bitmovin.player.api.event.listener.OnErrorListener
 
-class BitmovinErrorDetailsAdapter(private val player: BitmovinPlayer): ErrorDetailsAdapter() {
+class BitmovinErrorDetailsAdapter(private val player: BitmovinPlayer): EventSource<ErrorDetailsEventListener> {
+    private val eventEmitter: EventEmitter = EventEmitter()
     private val onErrorListener = OnErrorListener {
-        on { listener -> listener.onError(it.timestamp, it.code, it.message, it.data as? Throwable)}
+        eventEmitter.emit<ErrorDetailsEventListener> { listener -> listener.onError(it.timestamp, it.code, it.message, it.data as? Throwable)}
     }
 
     init {
@@ -18,5 +22,13 @@ class BitmovinErrorDetailsAdapter(private val player: BitmovinPlayer): ErrorDeta
 
     fun unwireEvents() {
         player.removeEventListener(onErrorListener)
+    }
+
+    override fun addEventListener(listener: ErrorDetailsEventListener) {
+        eventEmitter.addEventListener(listener)
+    }
+
+    override fun removeEventListener(listener: ErrorDetailsEventListener) {
+        eventEmitter.removeEventListener(listener)
     }
 }
--- collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/features/BitmovinSegmentTrackingAdapter.kt
+++ collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/features/BitmovinSegmentTrackingAdapter.kt
@@ -1,18 +1,21 @@
 package com.bitmovin.analytics.bitmovin.player.features
 
+import com.bitmovin.analytics.features.EventEmitter
+import com.bitmovin.analytics.features.EventSource
 import com.bitmovin.analytics.features.segmenttracking.DownloadFinishedEvent
 import com.bitmovin.analytics.features.segmenttracking.SegmentInfo
+import com.bitmovin.analytics.features.segmenttracking.SegmentTrackingEventListener
 import com.bitmovin.analytics.features.segmenttracking.SegmentType
 import com.bitmovin.player.BitmovinPlayer
 import com.bitmovin.player.api.event.listener.OnDownloadFinishedListener
 import com.bitmovin.player.config.network.HttpRequestType
 
-class BitmovinSegmentTrackingAdapter(private val player: BitmovinPlayer): SegmentTrackingAdapter() {
-
+class BitmovinSegmentTrackingAdapter(private val player: BitmovinPlayer): EventSource<SegmentTrackingEventListener> {
+    private val eventEmitter: EventEmitter = EventEmitter()
     private val onDownloadFinishedListener = OnDownloadFinishedListener {
         val segmentType = mapHttpRequestType(it.downloadType)
         val segmentInfo = SegmentInfo(it.timestamp, segmentType, it.url, it.lastRedirectLocation, it.httpStatus, it.downloadTime, it.size, it.isSuccess)
-        on { listener -> listener.onDownloadFinished(DownloadFinishedEvent(segmentInfo)) }
+        eventEmitter.emit<SegmentTrackingEventListener> { listener -> listener.onDownloadFinished(DownloadFinishedEvent(segmentInfo)) }
     }
 
     init {
@@ -37,5 +40,13 @@ class BitmovinSegmentTrackingAdapter(private val player: BitmovinPlayer): Segmen
 
     fun unwireEvents() {
         player.removeEventListener(onDownloadFinishedListener)
+    }
+
+    override fun addEventListener(listener: SegmentTrackingEventListener) {
+        eventEmitter.addEventListener(listener)
+    }
+
+    override fun removeEventListener(listener: SegmentTrackingEventListener) {
+        eventEmitter.removeEventListener(listener)
     }
 }
