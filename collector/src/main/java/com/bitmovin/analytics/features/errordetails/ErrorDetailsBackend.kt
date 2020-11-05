cf8a742f5f0242190ccab6f461a9e994aced7681
Roland Griesser <roland.griesser@bitmovin.com>
2020-11-05 11:16:27 +0100

    Implemented backend

--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailsBackend.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailsBackend.kt
@@ -1,15 +1,14 @@
 package com.bitmovin.analytics.features.errordetails
 
 import android.content.Context
+import com.bitmovin.analytics.utils.DataSerializer
 import com.bitmovin.analytics.utils.HttpClient
 
 class ErrorDetailsBackend(context: Context) {
     private val backendUrl = "https://analytics-ingress-global.bitmovin.com/errordetails"
-    private val TAG = "ErrorDetailsBackend"
     private val httpClient = HttpClient(context)
 
-
     fun send(errorDetails: ErrorDetails) {
-
+        httpClient.post(backendUrl, DataSerializer.serialize(errorDetails), null)
     }
 }
