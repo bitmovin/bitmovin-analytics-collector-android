26bc639005fec9ad38f66a85607098895478ff14
Peter Pfaffeneder <peter.pfaffeneder@gmail.com>
2020-11-25 10:11:11 +0100

    Added/fixed tests

--- collector/src/test/java/com/bitmovin/analytics/DeviceInformationDtoDataSerializationTest.kt
+++ collector/src/test/java/com/bitmovin/analytics/DeviceInformationDtoDataSerializationTest.kt
@@ -1,22 +1,41 @@
 package com.bitmovin.analytics
 
 import com.bitmovin.analytics.data.DeviceInformation
+import com.bitmovin.analytics.data.DeviceInformationProvider
 import com.bitmovin.analytics.data.EventData
+import com.bitmovin.analytics.data.decorators.DeviceInformationEventDataDecorator
 import com.bitmovin.analytics.utils.DataSerializer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.mockito.Mockito
 
 class DeviceInformationDtoDataSerializationTest {
     @Test
     fun testSerializesEventDataDeviceInformationCorrectly() {
+        //#region Mocking
         val config = BitmovinAnalyticsConfig("9ae0b480-f2ee-4c10-bc3c-cb88e982e0ac", "18ca6ad5-9768-4129-bdf6-17685e0d14d2")
-        val model = "Pixel 3"
-        val manufacturer = "Google"
-        val data = EventData(config, "1234", DeviceInformation(manufacturer, model, false, "user-agent", "de", "my.package", 100, 200), "user-id")
+        config.experimentName = "experimentName"
+        val data = EventData(config, "1234", "user-id")
+
+        val deviceInformationProviderMock = Mockito.mock(DeviceInformationProvider::class.java)
+        val deviceInformation = DeviceInformation("myManufacturer", "myModel", false, "user-agent", "de", "package-name", 100, 200)
+        Mockito.`when`(deviceInformationProviderMock.getDeviceInformation()).thenReturn(deviceInformation)
+
+        val decorator = DeviceInformationEventDataDecorator(deviceInformationProviderMock)
+        decorator.decorate(data)
+
         val serialized = DataSerializer.serialize(data)
+        //#endregion
 
         assertThat(serialized).contains("\"deviceInformation\":{")
-        assertThat(serialized).contains(String.format("\"model\":\"%s\"", data.deviceInformation.model))
-        assertThat(serialized).contains(String.format("\"manufacturer\":\"%s\"", data.deviceInformation.manufacturer))
+        assertThat(serialized).contains(String.format("\"model\":\"%s\"", "myModel"))
+        assertThat(serialized).contains(String.format("\"manufacturer\":\"%s\"", "myManufacturer"))
+        assertThat(serialized).contains(String.format("\"userAgent\":\"%s\"", "user-agent"))
+        assertThat(serialized).contains(String.format("\"platform\":\"%s\"", "android"))
+        assertThat(serialized).contains(String.format("\"language\":\"%s\"", "de"))
+        assertThat(serialized).contains(String.format("\"domain\":\"%s\"", "package-name"))
+        assertThat(serialized).contains(String.format("\"isTV\":%s", false))
+        assertThat(serialized).contains(String.format("\"screenHeight\":%s", 100))
+        assertThat(serialized).contains(String.format("\"screenWidth\":%s", 200))
     }
 }
---
+++ collector/src/test/java/com/bitmovin/analytics/data/EventDataTest.kt
@@ -0,0 +1,33 @@
+package com.bitmovin.analytics.data
+
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.bitmovin.analytics.BitmovinAnalyticsConfig
+import com.bitmovin.analytics.enums.CDNProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class EventDataTest {
+    private lateinit var bitmovinAnalyticsConfig: BitmovinAnalyticsConfig
+
+    @Before
+    fun setup() {
+        bitmovinAnalyticsConfig = BitmovinAnalyticsConfig("9ae0b480-f2ee-4c10-bc3c-cb88e982e0ac", "18ca6ad5-9768-4129-bdf6-17685e0d14d2")
+        bitmovinAnalyticsConfig.cdnProvider = CDNProvider.AKAMAI
+        bitmovinAnalyticsConfig.videoId = "1234"
+    }
+
+    @Test
+    fun testEventDataContainsDeviceInformation() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val deviceInformationProvider = DeviceInformationProvider(appContext, "user-agent")
+        val deviceInfo = deviceInformationProvider.getDeviceInformation()
+
+        assertThat(deviceInfo.model).isEqualTo(Build.MODEL)
+        assertThat(deviceInfo.manufacturer).isEqualTo(Build.MANUFACTURER)
+    }
+}
