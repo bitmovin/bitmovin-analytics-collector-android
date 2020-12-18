626828002ef8e7411471c2172d47085a68222dc0
Ines Kulenovic <ines.kulenovic@bitmovin.com>
2020-12-18 12:18:03 +0100

    move constants from util

--- collector/src/main/java/com/bitmovin/analytics/retryBackend/RetryQueue.kt
+++ collector/src/main/java/com/bitmovin/analytics/retryBackend/RetryQueue.kt
@@ -1,8 +1,6 @@
 package com.bitmovin.analytics.retryBackend
 
 import android.util.Log
-import com.bitmovin.analytics.utils.Util
-import java.lang.Exception
 import java.util.Calendar
 import java.util.Date
 import java.util.concurrent.locks.ReentrantLock
@@ -12,8 +10,12 @@ import kotlin.math.pow
 class RetryQueue {
     private val TAG = "RetryQueue"
     private val lock = ReentrantLock()
+    private val MAX_RETRY_TIME = 300 // in seconds
+    private val MAX_RETRY_SAMPLES = 100
+    private val MAX_BACKOFF_INTERVAL = 100
 
-    fun getMaxSampleNumber() = Util.MAX_RETRY_SAMPLES
+    fun getMaxSampleNumber() = MAX_RETRY_SAMPLES
+    fun now() = Date()
 
     private var retrySamplesList = mutableListOf<RetrySample<Any>>()
     private val sampleComparator = Comparator<RetrySample<Any>> { a, b ->
@@ -25,28 +27,29 @@ class RetryQueue {
     }
 
     fun addSample(retrySample: RetrySample<Any>) {
-
         try {
             lock.lock()
             retrySample.retry++
 
-            val backOffTime = minOf(2.toDouble().pow(retrySample.retry).toInt(), 64)
+            val backOffTime = minOf(2.toDouble().pow(retrySample.retry).toInt(), MAX_BACKOFF_INTERVAL)
             retrySample.totalTime += backOffTime
-            // more than 5min in queue
-            if (retrySample.totalTime < Util.MAX_RETRY_TIME) {
-                retrySample.scheduledTime = Calendar.getInstance().run {
-                    add(Calendar.SECOND, backOffTime)
-                    time
-                }
 
-                if (retrySamplesList.size > getMaxSampleNumber()) {
-                    val removeSample = retrySamplesList.last()
-                    retrySamplesList.remove(removeSample)
-                    Log.d(TAG, "removed sample")
-                }
-                retrySamplesList.add(retrySample)
-                retrySamplesList.sortWith(sampleComparator)
+            // more than 5min in queue
+            if (retrySample.totalTime > MAX_RETRY_TIME) {
+                return
             }
+
+            retrySample.scheduledTime = Calendar.getInstance().run {
+                add(Calendar.SECOND, backOffTime)
+                time
+            }
+            if (retrySamplesList.size >= getMaxSampleNumber()) {
+                val removeSample = retrySamplesList.last()
+                retrySamplesList.remove(removeSample)
+                Log.d(TAG, "removed sample")
+            }
+            retrySamplesList.add(retrySample)
+            retrySamplesList.sortWith(sampleComparator)
         } catch (e: Exception) {
             Log.d(TAG, "addSample ${e.message}")
         } finally {
@@ -57,7 +60,7 @@ class RetryQueue {
     fun getNextSampleOrNull(): RetrySample<Any>? {
         try {
             lock.lock()
-            val retrySample = retrySamplesList.firstOrNull { it.scheduledTime <= Date() }
+            val retrySample = retrySamplesList.firstOrNull { it.scheduledTime <= now() }
             retrySamplesList.remove(retrySample)
             return retrySample
         } catch (e: Exception) {
