cf2dc9f459d028a391e1489e0071b4b57d1b2ad0
Roland Griesser <roland.griesser@bitmovin.com>
2021-01-29 10:24:42 +0100

    Disabled features for now

--- collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/features/BitmovinFeatureFactory.kt
+++ collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/features/BitmovinFeatureFactory.kt
@@ -11,11 +11,12 @@ import com.bitmovin.player.BitmovinPlayer
 class BitmovinFeatureFactory(private val analytics: BitmovinAnalytics, private val player: BitmovinPlayer, private val context: Context) : FeatureFactory {
     override fun createFeatures(): Collection<Feature<*>> {
         val features = mutableListOf<Feature<*>>()
-        val segmentTrackingAdapter = BitmovinSegmentTrackingAdapter(player, analytics)
-        val segmentTracking = SegmentTracking(segmentTrackingAdapter)
-        features.add(segmentTracking)
-        val errorDetailsAdapter = BitmovinErrorDetailsAdapter(player, analytics)
-        features.add(ErrorDetailTracking(context, segmentTracking, errorDetailsAdapter, analytics))
+//        TODO features are disabled for now
+//        val segmentTrackingAdapter = BitmovinSegmentTrackingAdapter(player, analytics)
+//        val segmentTracking = SegmentTracking(segmentTrackingAdapter)
+//        features.add(segmentTracking)
+//        val errorDetailsAdapter = BitmovinErrorDetailsAdapter(player, analytics)
+//        features.add(ErrorDetailTracking(context, segmentTracking, errorDetailsAdapter, analytics))
         return features
     }
 }
