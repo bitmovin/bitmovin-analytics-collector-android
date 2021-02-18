c85b1702dcb7e6c47b8a72bdb69e0e9622d53a7c
Roland Griesser <roland.griesser@bitmovin.com>
2021-02-18 13:34:39 -0500

    Added concrete implementation of Observable

--- collector/src/main/java/com/bitmovin/analytics/features/segmenttracking/OnDownloadFinishedEventListener.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/segmenttracking/OnDownloadFinishedEventListener.kt
@@ -7,6 +7,29 @@ interface Observable<TListener> {
     fun unsubscribe(listener: TListener)
 }
 
+interface EventListenerNotifier<T> { fun notify(listener: T) }
+
+class EventSource<TListener>: Observable<TListener> {
+    private val listeners = mutableListOf<TListener>()
+
+    override fun subscribe(listener: TListener) {
+        listeners.add(listener)
+    }
+
+    override fun unsubscribe(listener: TListener) {
+        listeners.remove(listener)
+    }
+
+    // Helper method for inline Java calls
+    fun notify(action: EventListenerNotifier<TListener>) {
+        notify { action.notify(it) }
+    }
+
+    fun notify(action: (listener: TListener) -> Unit) {
+        listeners.forEach(action)
+    }
+}
+
 interface OnDownloadFinishedEventSource: Observable<OnDownloadFinishedEventListener>
 
 interface OnDownloadFinishedEventListener : EventListener {
