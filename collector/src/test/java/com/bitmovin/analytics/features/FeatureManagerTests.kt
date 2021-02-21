8bc52b9f034a174fe1dad77f248f2af9d17f92c7
Roland Griesser <roland.griesser@bitmovin.com>
2021-02-20 20:13:07 -0500

    Create FeatureManagerTests.kt

---
+++ collector/src/test/java/com/bitmovin/analytics/features/FeatureManagerTests.kt
@@ -0,0 +1,92 @@
+package com.bitmovin.analytics.features
+
+import io.mockk.Called
+import io.mockk.spyk
+import io.mockk.verify
+import io.mockk.verifyOrder
+import org.junit.Test
+
+class FeatureManagerTests {
+    class FeatureConfig1: FeatureConfig() {
+        var param1: String? = null
+    }
+    class FeatureConfig2: FeatureConfig()
+
+    @Test
+    fun testShouldConfigureAllFeaturesBeforeCallingEnabledHook() {
+        val feature1 = spyk(object: Feature<FeatureConfig1>("feature1", FeatureConfig1::class) {})
+        val feature2 = spyk(object: Feature<FeatureConfig2>("feature2", FeatureConfig2::class) {})
+
+        val featureManager = FeatureManager()
+        featureManager.registerFeature(feature1)
+        featureManager.registerFeature(feature2)
+        featureManager.configureFeatures(true, mapOf("feature1" to "{\"enabled\": true}", "feature2" to "{\"enabled\": true}"))
+        verifyOrder {
+            feature1.configured(any(), any())
+            feature2.configured(any(), any())
+            feature1.enabled()
+            feature2.enabled()
+        }
+    }
+
+    @Test
+    fun testShouldDisableFeatureIfNotAuthenticated() {
+        testShouldDisableFeature(false, mapOf())
+    }
+
+    @Test
+    fun testShouldDisableFeatureIfConfigIsntProvided() {
+        testShouldDisableFeature(true, mapOf())
+    }
+
+    @Test
+    fun testShouldDisableFeatureIfConfigDoesntContainEnabled() {
+        testShouldDisableFeature(true, mapOf("feature1" to "{}"))
+    }
+
+    @Test
+    fun testShouldDisableFeatureIfConfigHasEnabledFalse() {
+        testShouldDisableFeature(true, mapOf("feature1" to "{\"enabled\": false}"))
+    }
+
+    @Test
+    fun testShouldDisableFeatureIfConfigCantBeDeserialized() {
+        testShouldDisableFeature(true, mapOf("feature1" to "0"))
+    }
+
+    @Test
+    fun testShouldEnableFeatureIfConfigIsPresent() {
+        testShouldEnableFeature(mapOf("feature1" to "{\"enabled\": true, \"param1\": null}"))
+    }
+
+    @Test
+    fun testShouldEnableFeatureIfConfigIsPresentButNotComplete() {
+        testShouldEnableFeature(mapOf("feature1" to "{\"enabled\": true}"))
+    }
+
+    private fun testShouldDisableFeature(authenticated: Boolean, settings: Map<String, String>) {
+        val feature1 = spyk(object: Feature<FeatureConfig1>("feature1", FeatureConfig1::class) {})
+
+        val featureManager = FeatureManager()
+        featureManager.registerFeature(feature1)
+        featureManager.configureFeatures(authenticated, settings)
+        verifyOrder {
+            feature1.configure(any(), any())
+            feature1.disabled()
+        }
+        verify(exactly = 0) { feature1.enabled() }
+    }
+
+    private fun testShouldEnableFeature(settings: Map<String, String>) {
+        val feature1 = spyk(object: Feature<FeatureConfig1>("feature1", FeatureConfig1::class) {})
+
+        val featureManager = FeatureManager()
+        featureManager.registerFeature(feature1)
+        featureManager.configureFeatures(true, settings)
+        verifyOrder {
+            feature1.configured(any(), any())
+            feature1.enabled()
+        }
+        verify(exactly = 0) { feature1.disabled() }
+    }
+}
