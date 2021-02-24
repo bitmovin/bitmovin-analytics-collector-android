41a9b7bb570304cba8f25895c8204acaae4ce2d0
Roland Griesser <roland.griesser@bitmovin.com>
2021-02-24 17:03:16 -0500

    Made maxSegments optional

--- collector/src/main/java/com/bitmovin/analytics/features/segmenttracking/SegmentTrackingConfig.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/segmenttracking/SegmentTrackingConfig.kt
@@ -3,5 +3,5 @@ package com.bitmovin.analytics.features.segmenttracking
 import com.bitmovin.analytics.features.FeatureConfig
 
 class SegmentTrackingConfig : FeatureConfig() {
-    val maxSegments: Int = 10
+    val maxSegments: Int? = null
 }
