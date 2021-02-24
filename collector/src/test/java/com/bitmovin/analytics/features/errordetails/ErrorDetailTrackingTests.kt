b539bc145978149c80e9e1897f78f8e56169db2f
Roland Griesser <roland.griesser@bitmovin.com>
2021-02-24 18:21:22 -0500

    Update ErrorDetailTrackingTests.kt

--- collector/src/test/java/com/bitmovin/analytics/features/errordetails/ErrorDetailTrackingTests.kt
+++ collector/src/test/java/com/bitmovin/analytics/features/errordetails/ErrorDetailTrackingTests.kt
@@ -1,7 +1,18 @@
 package com.bitmovin.analytics.features.errordetails
 
+import com.bitmovin.analytics.ObservableSupport
+import com.bitmovin.analytics.features.segmenttracking.OnDownloadFinishedEventListener
+import com.bitmovin.analytics.features.segmenttracking.OnDownloadFinishedEventObject
 import com.bitmovin.analytics.features.segmenttracking.Segment
+import com.bitmovin.analytics.features.segmenttracking.SegmentTracking
 import com.bitmovin.analytics.features.segmenttracking.SegmentType
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.slot
+import io.mockk.spyk
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
@@ -30,5 +41,94 @@ class ErrorDetailTrackingTests {
         errorDetail.limitSegments(1)
         assertThat(errorDetail.segments?.get(0)).isEqualTo(segment1)
         assertThat(errorDetail.segments?.get(0)).isNotEqualTo(segment2)
+    }
+
+    @Test
+    fun testSuccessfullySubscribesToMultipleSources() {
+        val backend = mockk<ErrorDetailBackend>(relaxed = true)
+        val support1 = ObservableSupport<OnErrorDetailEventListener>()
+        val support2 = ObservableSupport<OnErrorDetailEventListener>()
+        val errorDetailTracking = ErrorDetailTracking(backend, null, support1, support2)
+        mockkObject(errorDetailTracking)
+        support1.notify { it.onError(0, null, null, null) }
+        support1.notify { it.onError(0, null, null, null) }
+        support2.notify { it.onError(0, null, null, null) }
+        support2.notify { it.onError(0, null, null, null) }
+
+        verify(exactly = 4) { errorDetailTracking.onError(any(), any(), any(), any()) }
+    }
+
+    @Test
+    fun testSuccessfullyUnsubscribesFromSourcesAndClearsBackendOnDisabling() {
+        val support = ObservableSupport<OnErrorDetailEventListener>()
+        val backend = mockk<ErrorDetailBackend>(relaxed = true)
+        val errorDetailTracking = ErrorDetailTracking(backend, null, support)
+        mockkObject(errorDetailTracking)
+        support.notify { it.onError(0, null, null, null) }
+        verify { errorDetailTracking.onError(any(), any(), any(), any()) }
+        clearMocks(errorDetailTracking)
+        errorDetailTracking.disable()
+        verify { backend.clear() }
+        support.notify { it.onError(0, null, null, null) }
+        verify(exactly = 0) { errorDetailTracking.onError(any(), any(), any(), any()) }
+    }
+
+    @Test
+    fun testEnablesAndFlushesBackendAfterEnablingFeature() {
+        val support = ObservableSupport<OnErrorDetailEventListener>()
+        val backend = mockk<ErrorDetailBackend>(relaxed = true)
+        val errorDetailTracking = ErrorDetailTracking(backend, null, support)
+        errorDetailTracking.enabled()
+        verify { backend.enabled = true }
+        verify { backend.flush() }
+    }
+
+    @Test
+    fun testLimitsQueuedItemsAfterEnablingFeature() {
+        val support = ObservableSupport<OnErrorDetailEventListener>()
+        val backend = mockk<ErrorDetailBackend>(relaxed = true)
+        val segmentTracking = SegmentTracking()
+        val errorDetailTracking = ErrorDetailTracking(backend, segmentTracking, support)
+        errorDetailTracking.enabled()
+        verify { backend.limitSegmentsInQueue(segmentTracking.maxSegments) }
+    }
+
+    @Test
+    fun testLimitsQueuedItemsToZeroIfSegmentTrackingIsDisabledAfterEnablingFeature() {
+        val support = ObservableSupport<OnErrorDetailEventListener>()
+        val backend = mockk<ErrorDetailBackend>(relaxed = true)
+        val segmentTracking = SegmentTracking()
+        segmentTracking.disable()
+        val errorDetailTracking = ErrorDetailTracking(backend, segmentTracking, support)
+        errorDetailTracking.enabled()
+        verify { backend.limitSegmentsInQueue(0) }
+    }
+
+    @Test
+    fun testAddsSegmentsOnError() {
+        val backend = mockk<ErrorDetailBackend>(relaxed = true)
+        val segmentTracking = SegmentTracking()
+        segmentTracking.onDownloadFinished(OnDownloadFinishedEventObject(mockk()))
+        segmentTracking.onDownloadFinished(OnDownloadFinishedEventObject(mockk()))
+        val errorDetailTracking = ErrorDetailTracking(backend, segmentTracking)
+        errorDetailTracking.onError(0, null, null, null)
+        val slot = slot<ErrorDetail>()
+        verify { backend.send(capture(slot)) }
+        assertThat(slot.captured.segments).isNotNull
+        assertThat(slot.captured.segments?.size).isEqualTo(2)
+    }
+
+    @Test
+    fun testDoesntAddSegmentsOnErrorIfSegmentTrackingIsDisabled() {
+        val backend = mockk<ErrorDetailBackend>(relaxed = true)
+        val segmentTracking = SegmentTracking()
+        segmentTracking.onDownloadFinished(OnDownloadFinishedEventObject(mockk()))
+        segmentTracking.onDownloadFinished(OnDownloadFinishedEventObject(mockk()))
+        segmentTracking.disable()
+        val errorDetailTracking = ErrorDetailTracking(backend, segmentTracking)
+        errorDetailTracking.onError(0, null, null, null)
+        val slot = slot<ErrorDetail>()
+        verify { backend.send(capture(slot)) }
+        assertThat(slot.captured.segments?.size ?: 0).isEqualTo(0)
     }
 }
