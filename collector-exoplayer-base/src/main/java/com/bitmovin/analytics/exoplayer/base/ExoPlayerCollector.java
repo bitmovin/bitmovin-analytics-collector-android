6391f99a7461848ece5f69ee92e3a40d7b4ec342
Ines Kulenovic <ines.kulenovic@bitmovin.com>
2021-03-22 14:59:48 +0100

    Moved basic files and test

---
+++ collector-exoplayer-base/src/main/java/com/bitmovin/analytics/exoplayer/base/ExoPlayerCollector.java
@@ -0,0 +1,43 @@
+package com.bitmovin.analytics.exoplayer.base;
+
+import android.content.Context;
+import com.bitmovin.analytics.BitmovinAnalytics;
+import com.bitmovin.analytics.BitmovinAnalyticsConfig;
+import com.bitmovin.analytics.data.DeviceInformationProvider;
+import com.google.android.exoplayer2.ExoPlayer;
+
+public class ExoPlayerCollector extends BitmovinAnalytics {
+
+    /**
+     * Bitmovin Analytics
+     *
+     * @param bitmovinAnalyticsConfig {@link BitmovinAnalyticsConfig}
+     * @param context {@link Context}
+     */
+    public ExoPlayerCollector(BitmovinAnalyticsConfig bitmovinAnalyticsConfig, Context context) {
+        super(bitmovinAnalyticsConfig, context);
+    }
+
+    /**
+     * Bitmovin Analytics
+     *
+     * @param bitmovinAnalyticsConfig {@link BitmovinAnalyticsConfig}
+     */
+    @Deprecated
+    public ExoPlayerCollector(BitmovinAnalyticsConfig bitmovinAnalyticsConfig) {
+        this(bitmovinAnalyticsConfig, bitmovinAnalyticsConfig.getContext());
+    }
+
+    public void attachPlayer(ExoPlayer player) {
+        DeviceInformationProvider deviceInformationProvider =
+                new DeviceInformationProvider(context, ExoUtil.getUserAgent(context));
+        ExoPlayerAdapter adapter =
+                new ExoPlayerAdapter(
+                        player,
+                        this.bitmovinAnalyticsConfig,
+                        deviceInformationProvider,
+                        this.playerStateMachine);
+
+        this.attach(adapter);
+    }
+}
---
+++ collector-exoplayer-base/src/main/java/com/bitmovin/analytics/exoplayer/base/ExoUtil.java
@@ -0,0 +1,44 @@
+package com.bitmovin.analytics.exoplayer.base;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import com.google.android.exoplayer2.ExoPlayerLibraryInfo;
+import com.google.android.exoplayer2.Player;
+import java.lang.reflect.Field;
+
+public class ExoUtil {
+    public static String exoStateToString(int state) {
+        switch (state) {
+            case Player.STATE_IDLE:
+                return "Idle";
+            case Player.STATE_BUFFERING:
+                return "Buffering";
+            case Player.STATE_READY:
+                return "Ready";
+            case Player.STATE_ENDED:
+                return "Ended";
+            default:
+                return "Unknown PlayerState";
+        }
+    }
+
+    public static String getPlayerVersion() {
+        try {
+            Field versionField = ExoPlayerLibraryInfo.class.getField("VERSION");
+            return (String) versionField.get(null);
+        } catch (NoSuchFieldException e) {
+        } catch (IllegalAccessException e) {
+        }
+        return "unknown";
+    }
+
+    public static String getUserAgent(Context context) {
+        ApplicationInfo applicationInfo = context.getApplicationInfo();
+        int stringId = applicationInfo.labelRes;
+        String applicationName = "Unknown";
+        if (stringId == 0 && applicationInfo.nonLocalizedLabel != null) {
+            applicationInfo.nonLocalizedLabel.toString();
+        }
+        return com.google.android.exoplayer2.util.Util.getUserAgent(context, applicationName);
+    }
+}
