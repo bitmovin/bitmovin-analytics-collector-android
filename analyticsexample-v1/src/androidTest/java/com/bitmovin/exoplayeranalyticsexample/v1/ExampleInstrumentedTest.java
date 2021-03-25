e3d1bf496ae219590af830a44e11b0ed9f0a8cf9
Ines Kulenovic <ines.kulenovic@bitmovin.com>
2021-03-25 16:05:35 +0100

    Changed formatting

--- analyticsexample-v1/src/androidTest/java/com/bitmovin/exoplayeranalyticsexample/v1/ExampleInstrumentedTest.java
+++ analyticsexample-v1/src/androidTest/java/com/bitmovin/exoplayeranalyticsexample/v1/ExampleInstrumentedTest.java
@@ -1,14 +1,12 @@
 package com.bitmovin.exoplayeranalyticsexample.v1;
 
+import static org.junit.Assert.*;
+
 import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
+import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
