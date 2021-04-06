fbf516d60df2ad891926a3f06e1f50a98ecb24d1
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2021-04-06 12:04:57 +0200

    usage of BitmovinAnalyticsSourceConfig in app

--- collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/config/BitmovinAnalyticsSourceConfig.kt
+++ collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/config/BitmovinAnalyticsSourceConfig.kt
@@ -2,24 +2,22 @@ package com.bitmovin.analytics.bitmovin.player.config
 
 import com.bitmovin.player.api.source.Source
 
-class BitmovinAnalyticsSourceConfig {
+class BitmovinAnalyticsSourceConfig(var playerSource: Source) {
 
-    val playerSource: Source? = null
+    var title: String? = null
+    var videoId: String? = null
+    var cdnProvider: String? = null
+    var experimentName: String? = null
+    var mpdUrl: String? = null
+    var m3u8Url: String? = null
+    var path: String? = null
+    var isLive: Boolean = false
 
-    val title: String? = null
-    val videoId: String? = null
-    val cdnProvider: String? = null
-    val experimentName: String? = null
-    val mpdUrl: String? = null
-    val m3u8Url: String? = null
-    val path: String? = null
-    val isLive: Boolean = false
-
-    val customData1: String? = null
-    val customData2: String? = null
-    val customData3: String? = null
-    val customData4: String? = null
-    val customData5: String? = null
-    val customData6: String? = null
-    val customData7: String? = null
+    var customData1: String? = null
+    var customData2: String? = null
+    var customData3: String? = null
+    var customData4: String? = null
+    var customData5: String? = null
+    var customData6: String? = null
+    var customData7: String? = null
 }
