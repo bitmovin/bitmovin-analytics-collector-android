e836cf8ec6b47c023e0f11a154fa798714be0bb0
Peter Petraník <peter.petranik@bitmovin.com>
2021-04-08 11:28:17 +0200

    use full package name

--- collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/BitmovinUtil.java
+++ collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/BitmovinUtil.java
@@ -1,13 +1,12 @@
 package com.bitmovin.analytics.bitmovin.player;
 
-import com.bitmovin.player.BuildConfig;
 import java.lang.reflect.Field;
 
 public class BitmovinUtil {
 
     public static String getPlayerVersion() {
         try {
-            Field versionField = BuildConfig.class.getField("VERSION_NAME");
+            Field versionField = com.bitmovin.player.BuildConfig.class.getField("VERSION_NAME");
             return (String) versionField.get(null);
         } catch (NoSuchFieldException e) {
         } catch (IllegalAccessException e) {
