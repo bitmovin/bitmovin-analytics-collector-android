e8588aac02311e4fca33bbd3cf131b1c1e27d196
Peter Petraník <peter.petranik@bitmovin.com>
2021-04-15 10:19:31 +0200

    add test case

--- collector-bitmovin-player/src/test/java/com/bitmovin/analytics/bitmovin/player/BitmovinSdkAdapterTest.kt
+++ collector-bitmovin-player/src/test/java/com/bitmovin/analytics/bitmovin/player/BitmovinSdkAdapterTest.kt
@@ -71,4 +71,29 @@ class BitmovinSdkAdapterTest {
         verify(exactly = 1) { playerStateMachine.transitionState(PlayerState.QUALITYCHANGE, any()) }
         verify(exactly = 1) { playerStateMachine.transitionState(PlayerState.PLAYING, any()) }
     }
+
+    @Test
+    fun `playerEventAudioPlaybackQualityChangedListener doesn't change playerStateMachine, when AudioPlaybackQualityChanged event with same bitrate is triggered`() {
+        // arrange
+        val listenerSlot = slot<EventListener<PlayerEvent.AudioPlaybackQualityChanged>>()
+        every { player.on(PlayerEvent.AudioPlaybackQualityChanged::class.java, capture(listenerSlot)) } answers { }
+        every { player.currentTime } returns 0.0
+        every { playerStateMachine.currentState } returns PlayerState.PLAYING
+        every { playerStateMachine.isStartupFinished } returns true
+        every { playerStateMachine.isQualityChangeEventEnabled } returns true
+        every { playerStateMachine.transitionState(any(), any()) } answers {}
+
+        // act
+        bitmovinSdkAdapter.init()
+        val sameAudioQuality = AudioQuality("", "", 200, null)
+        val audioPlaybackQualityChangedEvent = PlayerEvent.AudioPlaybackQualityChanged(sameAudioQuality, sameAudioQuality)
+        listenerSlot.captured.onEvent(audioPlaybackQualityChangedEvent)
+
+        // asset
+        verify { playerStateMachine.currentState }
+        verify { playerStateMachine.isStartupFinished }
+        verify { playerStateMachine.isQualityChangeEventEnabled }
+
+        verify(exactly = 0) { playerStateMachine.transitionState(any(), any()) }
+    }
 }
