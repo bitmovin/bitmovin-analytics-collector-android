bbbe561c86a78a76837a27f2afbff76f2d90ab46
Bitmovin Release Automation <support@bitmovin.com>
2021-07-06 14:02:23 +0200

    Fixed calls to FeatureFactory

--- collector-bitmovin-player-v1/src/main/java/com/bitmovin/analytics/bitmovin/player/BitmovinPlayerCollector.java
+++ collector-bitmovin-player-v1/src/main/java/com/bitmovin/analytics/bitmovin/player/BitmovinPlayerCollector.java
@@ -32,7 +32,7 @@ public class BitmovinPlayerCollector extends BitmovinAnalytics {
     public void attachPlayer(BitmovinPlayer player) {
         DeviceInformationProvider deviceInformationProvider =
                 new DeviceInformationProvider(context, getUserAgent(context));
-        FeatureFactory featureFactory = new BitmovinFeatureFactory(this, player, context);
+        FeatureFactory featureFactory = new BitmovinFeatureFactory(bitmovinAnalyticsConfig, this, player, context);
         BitmovinSdkAdapter adapter =
                 new BitmovinSdkAdapter(
                         player,
