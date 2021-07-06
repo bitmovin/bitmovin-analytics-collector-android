e9ecf62d75dfc5ca55d40990556f29a815f64304
Bitmovin Release Automation <support@bitmovin.com>
2021-07-06 13:54:08 +0200

    Added licenseKey, impressionId and errorId to errorDetail DTO

--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetail.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetail.kt
@@ -1,8 +1,9 @@
 package com.bitmovin.analytics.features.errordetails
 
 import com.bitmovin.analytics.features.segmenttracking.Segment
+import java.util.*
 
-data class ErrorDetail(val timestamp: Long, val code: Int?, val message: String?, val stacktrace: Collection<String>?, val segments: MutableList<Segment>?) {
+data class ErrorDetail(val licenseKey: String, val impressionId: String, val errorId: String, val timestamp: Long, val code: Int?, val message: String?, val stacktrace: Collection<String>?, val segments: MutableList<Segment>?) {
     fun limitSegments(max: Int) {
         segments ?: return
         while (segments.size > max) {
--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailTracking.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailTracking.kt
@@ -1,14 +1,18 @@
 package com.bitmovin.analytics.features.errordetails
 
+import com.bitmovin.analytics.BitmovinAnalyticsConfig
+import com.bitmovin.analytics.ImpressionIdProvider
+import com.bitmovin.analytics.LicenseKeyProvider
 import com.bitmovin.analytics.Observable
 import com.bitmovin.analytics.features.Feature
 import com.bitmovin.analytics.features.segmenttracking.SegmentTracking
+import com.bitmovin.analytics.utils.Util
 import com.bitmovin.analytics.utils.topOfStacktrace
+import java.util.*
 
-class ErrorDetailTracking(private val backend: ErrorDetailBackend, private val segmentTracking: SegmentTracking?, private vararg val observables: Observable<OnErrorDetailEventListener>) :
+class ErrorDetailTracking(private val analyticsConfig: BitmovinAnalyticsConfig, private val impressionIdProvider: ImpressionIdProvider, private val backend: ErrorDetailBackend, private val segmentTracking: SegmentTracking?, private vararg val observables: Observable<OnErrorDetailEventListener>) :
         Feature<ErrorDetailTrackingConfig>("errorDetails", ErrorDetailTrackingConfig::class),
         OnErrorDetailEventListener {
-
     init {
         observables.forEach { it.subscribe(this) }
     }
@@ -23,6 +27,9 @@ class ErrorDetailTracking(private val backend: ErrorDetailBackend, private val s
         backend.flush()
     }
 
+    //TODO reset errors, reset segments when new impression id
+    // info: no relation between error sample in normal impression and error detail as we can also have other errors (from analytics e.g.)
+
     override fun disabled() {
         backend.clear()
         observables.forEach { it.unsubscribe(this) }
@@ -33,7 +40,7 @@ class ErrorDetailTracking(private val backend: ErrorDetailBackend, private val s
             return
         }
         val segments = segmentTracking?.segments?.toMutableList()
-        val errorDetails = ErrorDetail(timestamp, code, message, throwable?.topOfStacktrace?.toList(), segments)
+        val errorDetails = ErrorDetail(analyticsConfig.key, impressionIdProvider.impressionId, Util.getUUID(), timestamp, code, message, throwable?.topOfStacktrace?.toList(), segments)
         backend.send(errorDetails)
     }
 }
