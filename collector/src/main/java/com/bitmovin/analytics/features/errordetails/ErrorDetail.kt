8d0500206ccadeee5f1430e2403220c4710b857e
Bitmovin Release Automation <support@bitmovin.com>
2021-07-07 10:13:40 +0200

    Added domain to ErrorDetail DTO

--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetail.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetail.kt
@@ -3,7 +3,7 @@ package com.bitmovin.analytics.features.errordetails
 import com.bitmovin.analytics.features.segmenttracking.Segment
 import java.util.*
 
-data class ErrorDetail(val licenseKey: String, val impressionId: String, val errorId: String, val timestamp: Long, val code: Int?, val message: String?, val stacktrace: Collection<String>?, val segments: MutableList<Segment>?) {
+data class ErrorDetail(val licenseKey: String, val domain: String, val impressionId: String, val errorId: Long, val timestamp: Long, val code: Int?, val message: String?, val stacktrace: Collection<String>?, val segments: MutableList<Segment>?) {
     fun limitSegments(max: Int) {
         segments ?: return
         while (segments.size > max) {
