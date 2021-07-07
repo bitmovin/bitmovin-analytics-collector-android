bb948a143724ca70b45e48e54baac7daaef7993d
Bitmovin Release Automation <support@bitmovin.com>
2021-07-07 14:24:03 +0200

    Adding additional segment types

--- collector/src/main/java/com/bitmovin/analytics/features/segmenttracking/SegmentType.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/segmenttracking/SegmentType.kt
@@ -3,6 +3,10 @@ package com.bitmovin.analytics.features.segmenttracking
 enum class SegmentType {
     DRM_LICENSE_WIDEVINE,
     MEDIA_THUMBNAILS,
+    MEDIA_VIDEO,
+    MEDIA_AUDIO,
+    MEDIA_PROGRESSIVE,
+    MEDIA_SUBTITLES,
     MANIFEST_DASH,
     MANIFEST_HLS_MASTER,
     MANIFEST_HLS_VARIANT,
