cbd2ea8383685d30eb05e8d497cb05a73e6c9970
Bitmovin Release Automation <support@bitmovin.com>
2021-07-08 14:40:14 +0200

    Cleaned up code

--- collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/features/BitmovinFeatureFactory.kt
+++ collector-bitmovin-player/src/main/java/com/bitmovin/analytics/bitmovin/player/features/BitmovinFeatureFactory.kt
@@ -8,16 +8,18 @@ import com.bitmovin.analytics.features.FeatureFactory
 import com.bitmovin.analytics.features.errordetails.ErrorDetailBackend
 import com.bitmovin.analytics.features.errordetails.ErrorDetailTracking
 import com.bitmovin.analytics.features.segmenttracking.SegmentTracking
+import com.bitmovin.analytics.license.FeatureConfigContainer
 import com.bitmovin.player.api.Player
 
 class BitmovinFeatureFactory(private val analyticsConfig: BitmovinAnalyticsConfig, private val analytics: BitmovinAnalytics, private val player: Player, private val context: Context) : FeatureFactory {
-    override fun createFeatures(): Collection<Feature<*>> {
-        val features = mutableListOf<Feature<*>>()
+    override fun createFeatures(): Collection<Feature<FeatureConfigContainer, *>> {
+        val features = mutableListOf<Feature<FeatureConfigContainer, *>>()
         val segmentTrackingAdapter = BitmovinSegmentTrackingAdapter(player, analytics.onAnalyticsReleasingObservable)
         val segmentTracking = SegmentTracking(segmentTrackingAdapter)
         val errorDetailsAdapter = BitmovinErrorDetailsAdapter(player, analytics.onAnalyticsReleasingObservable)
         val errorDetailsBackend = ErrorDetailBackend(analyticsConfig.config, context)
-        features.add(ErrorDetailTracking(context, analyticsConfig, analytics, errorDetailsBackend, segmentTracking, errorDetailsAdapter, analytics.onErrorDetailObservable))
+        var errorDetailTracking = ErrorDetailTracking(context, analyticsConfig, analytics, errorDetailsBackend, segmentTracking, errorDetailsAdapter, analytics.onErrorDetailObservable)
+        features.add(errorDetailTracking)
         return features
     }
 }
