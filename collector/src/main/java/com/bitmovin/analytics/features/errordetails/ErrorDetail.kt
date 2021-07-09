6ab26a62f189af901dc01604a2d89ed59555e04e
Bitmovin Release Automation <support@bitmovin.com>
2021-07-09 10:47:46 +0200

    Added analyticsVersion to ErrorDetail

--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetail.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetail.kt
@@ -3,6 +3,8 @@ package com.bitmovin.analytics.features.errordetails
 import com.bitmovin.analytics.features.segmenttracking.Segment
 
 data class ErrorDetail(val licenseKey: String, val domain: String, val impressionId: String, val errorId: Long, val timestamp: Long, val code: Int?, val message: String?, val stacktrace: Collection<String>?, val segments: MutableList<Segment>?) {
+    val analyticsVersion: String = com.bitmovin.analytics.BuildConfig.VERSION_NAME
+
     fun limitSegments(max: Int) {
         segments ?: return
         while (segments.size > max) {
