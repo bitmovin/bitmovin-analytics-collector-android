80baa145127147ed016cfeface6d3fa470bc57fc
Bitmovin Release Automation <support@bitmovin.com>
2021-07-20 17:45:17 +0200

    removed todo

--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailBackend.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailBackend.kt
@@ -48,8 +48,7 @@ class ErrorDetailBackend(collectorConfig: CollectorConfig, context: Context) {
     companion object {
         const val MAX_URL_LENGTH = 200
         const val MAX_STRING_LENGTH = 400
-
-        // TODO get platform from utilities
+        
         fun ErrorDetail.copyTruncateStringsAndUrls(maxStringLength: Int, maxUrlLength: Int): ErrorDetail = this.copy(
                 message = message?.take(maxStringLength),
                 segments = segments?.map {
