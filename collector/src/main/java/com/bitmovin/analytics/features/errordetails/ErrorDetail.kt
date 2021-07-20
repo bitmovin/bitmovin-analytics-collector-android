0f04df7def68c8a9cfa784b7c29c5c8986d8f977
Bitmovin Release Automation <support@bitmovin.com>
2021-07-20 17:18:38 +0200

    Made segments readonly and limiting on a copy instead

--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetail.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetail.kt
@@ -3,13 +3,6 @@ package com.bitmovin.analytics.features.errordetails
 import com.bitmovin.analytics.features.segmenttracking.Segment
 import com.bitmovin.analytics.utils.Util
 
-data class ErrorDetail(val platform: String, val licenseKey: String, val domain: String, val impressionId: String, val errorId: Long, val timestamp: Long, val code: Int?, val message: String?, val stacktrace: Collection<String>?, val segments: MutableList<Segment>?) {
-    val analyticsVersion: String = Util.getAnalyticsVersion()
-
-    fun limitSegments(max: Int) {
-        segments ?: return
-        while (segments.size > max) {
-            segments.removeAt(segments.size - 1)
-        }
-    }
+data class ErrorDetail(val platform: String, val licenseKey: String, val domain: String, val impressionId: String, val errorId: Long, val timestamp: Long, val code: Int?, val message: String?, val stacktrace: Collection<String>?, val segments: List<Segment>?) {
+    val analyticsVersion: String = com.bitmovin.analytics.BuildConfig.VERSION_NAME
 }
--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailBackend.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailBackend.kt
@@ -2,6 +2,7 @@ package com.bitmovin.analytics.features.errordetails
 
 import android.content.Context
 import com.bitmovin.analytics.CollectorConfig
+import com.bitmovin.analytics.features.segmenttracking.Segment
 import com.bitmovin.analytics.utils.DataSerializer
 import com.bitmovin.analytics.utils.HttpClient
 import com.bitmovin.analytics.utils.Util
@@ -11,13 +12,14 @@ import okhttp3.OkHttpClient
 class ErrorDetailBackend(collectorConfig: CollectorConfig, context: Context) {
     private val backendUrl = Util.joinUrl(collectorConfig.backendUrl, "/analytics/error")
     private val httpClient = HttpClient(context, OkHttpClient())
-    private val queue = LinkedList<ErrorDetail>()
+    private val _queue = LinkedList<ErrorDetail>()
+    val queue: List<ErrorDetail> = _queue
 
     var enabled: Boolean = false
 
     fun limitSegmentsInQueue(max: Int) {
-        queue.forEach {
-            it.limitSegments(max)
+        for ((index, detail) in _queue.withIndex()) {
+            _queue.set(index, detail.copyTruncateSegments(max))
         }
     }
 
@@ -32,13 +34,17 @@ class ErrorDetailBackend(collectorConfig: CollectorConfig, context: Context) {
     fun flush() {
         // We create a copy of the list to avoid side-effects like ending up in an infinite loop if we always add and remove the same element.
         // This shouldn't happen as Kotlin is call-by-value, so `send` would not modify the original queue.
-        queue.toList().forEach {
+        _queue.toList().forEach {
             send(it)
-            queue.remove(it)
+            _queue.remove(it)
         }
     }
 
     fun clear() {
-        queue.clear()
+        _queue.clear()
+    }
+
+    companion object {
+        fun ErrorDetail.copyTruncateSegments(maxSegments: Int) = this.copy(segments = segments?.take(maxSegments))
     }
 }
