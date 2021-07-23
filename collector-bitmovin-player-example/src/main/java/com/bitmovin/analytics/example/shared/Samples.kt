2ec23de175512b09e56a4de7727b67fc8d65526c
Bitmovin Release Automation <support@bitmovin.com>
2021-07-23 10:44:42 +0200

    code style

--- collector-bitmovin-player-example/src/main/java/com/bitmovin/analytics/example/shared/Samples.kt
+++ collector-bitmovin-player-example/src/main/java/com/bitmovin/analytics/example/shared/Samples.kt
@@ -3,12 +3,12 @@ package com.bitmovin.analytics.example.shared
 import android.net.Uri
 
 data class Sample(val name: String, val uri: Uri, val drmScheme: String? = null, val drmLicenseUri: Uri? = null) {
-    constructor(name: String, uri: String, drmScheme: String? = null, drmLicenseUri: String? = null)
-            : this(name, Uri.parse(uri), drmScheme, drmLicenseUri?.let { Uri.parse(it) })
+    constructor(name: String, uri: String, drmScheme: String? = null, drmLicenseUri: String? = null) :
+            this(name, Uri.parse(uri), drmScheme, drmLicenseUri?.let { Uri.parse(it) })
 }
 
-//https://github.com/google/ExoPlayer/blob/release-v1/demo/src/main/java/com/google/android/exoplayer/demo/Samples.java
-//https://github.com/google/ExoPlayer/blob/release-v2/demos/main/src/main/assets/media.exolist.json
+// https://github.com/google/ExoPlayer/blob/release-v1/demo/src/main/java/com/google/android/exoplayer/demo/Samples.java
+// https://github.com/google/ExoPlayer/blob/release-v2/demos/main/src/main/assets/media.exolist.json
 object Samples {
     val HLS = Sample("Hls", "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")
     val DASH = Sample("Hls", "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd")
