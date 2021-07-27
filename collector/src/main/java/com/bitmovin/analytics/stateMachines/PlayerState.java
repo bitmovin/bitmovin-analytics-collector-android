551e0505b422103725ec26a1652a376ac767a8a6
Bitmovin Release Automation <support@bitmovin.com>
2021-07-27 17:38:36 +0200

    Using static objects instead of PlayerState enum

--- collector/src/main/java/com/bitmovin/analytics/stateMachines/PlayerState.java
+++ collector/src/main/java/com/bitmovin/analytics/stateMachines/PlayerState.java
@@ -3,216 +3,256 @@ package com.bitmovin.analytics.stateMachines;
 import com.bitmovin.analytics.data.ErrorCode;
 import com.bitmovin.analytics.enums.AnalyticsErrorCodes;
 
-public enum PlayerState {
-    READY {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {}
+public class PlayerState
+{
+    public static IPlayerState<Void> READY =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {}
 
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState desintationPlayerState) {}
-    },
-    SOURCE_CHANGED {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {}
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {}
+            };
+    public static IPlayerState<Void> SOURCE_CHANGED =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {}
 
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState desintationPlayerState) {}
-    },
-    STARTUP {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {
-            machine.videoStartTimeout.start();
-        }
-
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState destinationPlayerState) {
-            machine.videoStartTimeout.cancel();
-            long elapsedTimeOnEnter = machine.getElapsedTimeOnEnter();
-            machine.addStartupTime(elapsedTime - elapsedTimeOnEnter);
-            if (destinationPlayerState == PlayerState.PLAYING) {
-                long playerStartupTime = machine.getAndResetPlayerStartupTime();
-                for (StateMachineListener listener : machine.getListeners()) {
-                    listener.onStartup(machine.getStartupTime(), playerStartupTime);
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> desintationPlayerState) {}
+            };
+    public static IPlayerState<Void> STARTUP =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {
+                    machine.videoStartTimeout.start();
                 }
-                machine.setStartupFinished(true);
-            }
-        }
-    },
-    AD {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {}
 
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState destinationPlayerState) {}
-    },
-    ADFINISHED {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {}
-
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState destinationPlayerState) {}
-    },
-    BUFFERING {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {
-            machine.enableRebufferHeartbeat();
-            machine.rebufferingTimeout.start();
-        }
-
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState desintationPlayerState) {
-            machine.disableRebufferHeartbeat();
-            for (StateMachineListener listener : machine.getListeners()) {
-                long elapsedTimeOnEnter = machine.getElapsedTimeOnEnter();
-                listener.onRebuffering(elapsedTime - elapsedTimeOnEnter);
-            }
-            machine.rebufferingTimeout.cancel();
-        }
-    },
-    ERROR {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {
-            machine.videoStartTimeout.cancel();
-            for (StateMachineListener listener : machine.getListeners()) {
-                listener.onError(machine.getErrorCode());
-            }
-        }
-
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState desintationPlayerState) {
-            machine.setVideoStartFailedReason(null);
-        }
-    },
-    EXITBEFOREVIDEOSTART {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {
-            for (StateMachineListener listener : machine.getListeners()) {
-                listener.onVideoStartFailed();
-            }
-        }
-
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState desintationPlayerState) {
-            machine.setVideoStartFailedReason(null);
-        }
-    },
-    PLAYING {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {
-            machine.enableHeartbeat();
-        }
-
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState desintationPlayerState) {
-            for (StateMachineListener listener : machine.getListeners()) {
-                long elapsedTimeOnEnter = machine.getElapsedTimeOnEnter();
-                listener.onPlayExit(elapsedTime - elapsedTimeOnEnter);
-            }
-
-            machine.disableHeartbeat();
-        }
-    },
-    PAUSE {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {}
-
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState desintationPlayerState) {
-            for (StateMachineListener listener : machine.getListeners()) {
-                long elapsedTimeOnEnter = machine.getElapsedTimeOnEnter();
-                listener.onPauseExit(elapsedTime - elapsedTimeOnEnter);
-            }
-        }
-    },
-    QUALITYCHANGE {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {
-            machine.increaseQualityChangeCount();
-            if (!machine.isQualityChangeTimerRunning) {
-                machine.qualityChangeResetTimeout.start();
-            }
-        }
-
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState destinationPlayerState) {
-            if (machine.isQualityChangeEventEnabled()) {
-                for (StateMachineListener listener : machine.getListeners()) {
-                    listener.onQualityChange();
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {
+                    machine.videoStartTimeout.cancel();
+                    long elapsedTimeOnEnter = machine.getElapsedTimeOnEnter();
+                    machine.addStartupTime(elapsedTime - elapsedTimeOnEnter);
+                    if (destinationPlayerState == PlayerState.PLAYING) {
+                        long playerStartupTime = machine.getAndResetPlayerStartupTime();
+                        for (StateMachineListener listener : machine.getListeners()) {
+                            listener.onStartup(machine.getStartupTime(), playerStartupTime);
+                        }
+                        machine.setStartupFinished(true);
+                    }
                 }
-            } else {
-                ErrorCode errorCode =
-                        AnalyticsErrorCodes.ANALYTICS_QUALITY_CHANGE_THRESHOLD_EXCEEDED
-                                .getErrorCode();
+            };
+    public static IPlayerState<Void> AD =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {}
 
-                for (StateMachineListener listener : machine.getListeners()) {
-                    listener.onError(errorCode);
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {}
+            };
+    public static IPlayerState<Void> ADFINISHED =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {}
+
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {}
+            };
+    public static IPlayerState<Void> BUFFERING =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {
+                    machine.enableRebufferHeartbeat();
+                    machine.rebufferingTimeout.start();
                 }
-            }
-        }
-    },
-    CUSTOMDATACHANGE {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {}
 
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState destinationPlayerState) {}
-    },
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> desintationPlayerState) {
+                    machine.disableRebufferHeartbeat();
+                    for (StateMachineListener listener : machine.getListeners()) {
+                        long elapsedTimeOnEnter = machine.getElapsedTimeOnEnter();
+                        listener.onRebuffering(elapsedTime - elapsedTimeOnEnter);
+                    }
+                    machine.rebufferingTimeout.cancel();
+                }
+            };
+    public static IPlayerState<Void> ERROR =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {
+                    machine.videoStartTimeout.cancel();
+                    for (StateMachineListener listener : machine.getListeners()) {
+                        listener.onError(machine.getErrorCode());
+                    }
+                }
 
-    AUDIOTRACKCHANGE {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {}
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> desintationPlayerState) {
+                    machine.setVideoStartFailedReason(null);
+                }
+            };
+    public static IPlayerState<Void> EXITBEFOREVIDEOSTART =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {
+                    for (StateMachineListener listener : machine.getListeners()) {
+                        listener.onVideoStartFailed();
+                    }
+                }
 
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState destinationPlayerState) {
-            for (StateMachineListener listener : machine.getListeners()) {
-                listener.onAudioTrackChange();
-            }
-        }
-    },
-    SUBTITLECHANGE {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {}
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> desintationPlayerState) {
+                    machine.setVideoStartFailedReason(null);
+                }
+            };
+    public static IPlayerState<Void> PLAYING =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {
+                    machine.enableHeartbeat();
+                }
 
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState destinationPlayerState) {
-            for (StateMachineListener listener : machine.getListeners()) {
-                listener.onSubtitleChange();
-            }
-        }
-    },
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> desintationPlayerState) {
+                    for (StateMachineListener listener : machine.getListeners()) {
+                        long elapsedTimeOnEnter = machine.getElapsedTimeOnEnter();
+                        listener.onPlayExit(elapsedTime - elapsedTimeOnEnter);
+                    }
 
-    SEEKING {
-        @Override
-        void onEnterState(PlayerStateMachine machine) {
-            machine.setElapsedTimeSeekStart(machine.getElapsedTimeOnEnter());
-        }
+                    machine.disableHeartbeat();
+                }
+            };
+    public static IPlayerState<Void> PAUSE =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {}
 
-        @Override
-        void onExitState(
-                PlayerStateMachine machine, long elapsedTime, PlayerState destinationPlayerState) {
-            for (StateMachineListener listener : machine.getListeners()) {
-                listener.onSeekComplete(elapsedTime - machine.getElapsedTimeSeekStart());
-            }
-            machine.setElapsedTimeSeekStart(0);
-        }
-    };
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> desintationPlayerState) {
+                    for (StateMachineListener listener : machine.getListeners()) {
+                        long elapsedTimeOnEnter = machine.getElapsedTimeOnEnter();
+                        listener.onPauseExit(elapsedTime - elapsedTimeOnEnter);
+                    }
+                }
+            };
+    public static IPlayerState<Void> QUALITYCHANGE =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {
+                    machine.increaseQualityChangeCount();
+                    if (!machine.isQualityChangeTimerRunning) {
+                        machine.qualityChangeResetTimeout.start();
+                    }
+                }
 
-    abstract void onEnterState(PlayerStateMachine machine);
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {
+                    if (machine.isQualityChangeEventEnabled()) {
+                        for (StateMachineListener listener : machine.getListeners()) {
+                            listener.onQualityChange();
+                        }
+                    } else {
+                        ErrorCode errorCode =
+                                AnalyticsErrorCodes.ANALYTICS_QUALITY_CHANGE_THRESHOLD_EXCEEDED
+                                        .getErrorCode();
 
-    abstract void onExitState(
-            PlayerStateMachine machine, long elapsedTime, PlayerState desintationPlayerState);
+                        for (StateMachineListener listener : machine.getListeners()) {
+                            listener.onError(errorCode);
+                        }
+                    }
+                }
+            };
+    public static IPlayerState<Void> CUSTOMDATACHANGE =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {}
+
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {}
+            };
+    public static IPlayerState<Void> AUDIOTRACKCHANGE =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {}
+
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {
+                    for (StateMachineListener listener : machine.getListeners()) {
+                        listener.onAudioTrackChange();
+                    }
+                }
+            };
+    public static IPlayerState<Void> SUBTITLECHANGE =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {}
+
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {
+                    for (StateMachineListener listener : machine.getListeners()) {
+                        listener.onSubtitleChange();
+                    }
+                }
+            };
+
+    public static IPlayerState<Void> SEEKING =
+            new IPlayerState<Void>() {
+                @Override
+                public void onEnterState(PlayerStateMachine machine, Void data) {
+                    machine.setElapsedTimeSeekStart(machine.getElapsedTimeOnEnter());
+                }
+
+                @Override
+                public void onExitState(
+                        PlayerStateMachine machine,
+                        long elapsedTime,
+                        IPlayerState<?> destinationPlayerState) {
+                    for (StateMachineListener listener : machine.getListeners()) {
+                        listener.onSeekComplete(elapsedTime - machine.getElapsedTimeSeekStart());
+                    }
+                    machine.setElapsedTimeSeekStart(0);
+                }
+            };
 }
