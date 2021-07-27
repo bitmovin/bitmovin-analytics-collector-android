4da178a2ba0f7161f1fb974af1ad293719222316
Bitmovin Release Automation <support@bitmovin.com>
2021-07-27 18:01:20 +0200

    Converted PlayerStates and PlayerState to Kotlin

--- collector/src/main/java/com/bitmovin/analytics/stateMachines/PlayerState.java
+++
@@ -1,8 +0,0 @@
-package com.bitmovin.analytics.stateMachines;
-
-public interface PlayerState<T> {
-    void onEnterState(PlayerStateMachine machine, T data);
-
-    void onExitState(
-            PlayerStateMachine machine, long elapsedTime, PlayerState<?> destinationPlayerState);
-}
