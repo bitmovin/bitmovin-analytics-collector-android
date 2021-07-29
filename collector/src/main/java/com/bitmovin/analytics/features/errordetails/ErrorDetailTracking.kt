a2f5b14e3bf8ba59a57783e82908eac2779f6052
Bitmovin Release Automation <support@bitmovin.com>
2021-07-29 08:30:05 +0200

    Made errorData optional

--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailTracking.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/ErrorDetailTracking.kt
@@ -44,7 +44,7 @@ class ErrorDetailTracking(private val context: Context, private val analyticsCon
         errorIndex = 0
     }
 
-    override fun onError(code: Int?, message: String?, errorData: ErrorData) {
+    override fun onError(code: Int?, message: String?, errorData: ErrorData?) {
         if (!isEnabled) {
             return
         }
@@ -54,7 +54,7 @@ class ErrorDetailTracking(private val context: Context, private val analyticsCon
         val platform = Util.getPlatform(Util.isTVDevice(context))
         val timestamp = Util.getTimestamp()
 
-        val errorDetails = ErrorDetail(platform, analyticsConfig.key, Util.getDomain(context), impressionIdProvider.impressionId, errorIndex, timestamp, code, message, errorData, segments)
+        val errorDetails = ErrorDetail(platform, analyticsConfig.key, Util.getDomain(context), impressionIdProvider.impressionId, errorIndex, timestamp, code, message, errorData ?: ErrorData(), segments)
         backend.send(errorDetails)
     }
 }
--- collector/src/main/java/com/bitmovin/analytics/features/errordetails/OnErrorDetailEventListener.kt
+++ collector/src/main/java/com/bitmovin/analytics/features/errordetails/OnErrorDetailEventListener.kt
@@ -1,5 +1,5 @@
 package com.bitmovin.analytics.features.errordetails
 
 interface OnErrorDetailEventListener {
-    fun onError(code: Int?, message: String?, errorData: ErrorData)
+    fun onError(code: Int?, message: String?, errorData: ErrorData?)
 }
