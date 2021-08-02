9e893ff1f800d91bc87a586ea9b522115410f4ab
Bitmovin Release Automation <support@bitmovin.com>
2021-08-02 14:32:49 +0200

    Created public Collector interface and a default implementation

---
+++ collector/src/main/java/com/bitmovin/analytics/Collector.kt
@@ -0,0 +1,16 @@
+package com.bitmovin.analytics
+
+import com.bitmovin.analytics.data.CustomData
+
+interface Collector<TPlayer> {
+    var customData: CustomData
+    val impressionId: String
+    val config: BitmovinAnalyticsConfig
+
+    fun attachPlayer(player: TPlayer)
+    fun detachPlayer()
+    fun setCustomDataOnce(customData: CustomData)
+
+    fun addDebugListener(listener: BitmovinAnalytics.DebugListener)
+    fun removeDebugListener(listener: BitmovinAnalytics.DebugListener)
+}
