89e88ebfdb27c06089fa8b06244b9f574af3fdf1
Roland Griesser <roland.griesser@bitmovin.com>
2021-12-06 16:28:00 +0100

    Converted PlayerAdapter to Kotlin

---
+++ collector/src/main/java/com/bitmovin/analytics/adapters/PlayerAdapter.kt
@@ -0,0 +1,24 @@
+package com.bitmovin.analytics.adapters
+
+import com.bitmovin.analytics.config.SourceMetadata
+import com.bitmovin.analytics.data.EventData
+import com.bitmovin.analytics.data.manipulators.EventDataManipulatorPipeline
+import com.bitmovin.analytics.features.Feature
+import com.bitmovin.analytics.license.FeatureConfigContainer
+import com.bitmovin.analytics.stateMachines.PlayerStateMachine
+
+interface PlayerAdapter {
+    val position: Long
+    val drmDownloadTime: Long?
+    val currentSourceMetadata: SourceMetadata?
+
+    fun init(): Collection<Feature<FeatureConfigContainer, *>>
+    fun release()
+    fun resetSourceRelatedState()
+    fun registerEventDataManipulators(pipeline: EventDataManipulatorPipeline)
+    fun clearValues()
+    fun createEventData(): EventData
+    fun createAdAdapter(): AdAdapter? {
+        return null
+    }
+}
