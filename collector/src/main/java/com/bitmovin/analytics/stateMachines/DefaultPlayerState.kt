b497c22a67b7955506f234d9aeccc0eccc1989f1
Roland Griesser <roland.griesser@bitmovin.com>
2021-12-14 12:16:01 +0100

    Added setting of data when entering the state

--- collector/src/main/java/com/bitmovin/analytics/stateMachines/DefaultPlayerState.kt
+++ collector/src/main/java/com/bitmovin/analytics/stateMachines/DefaultPlayerState.kt
@@ -1,7 +1,10 @@
 package com.bitmovin.analytics.stateMachines
 
 open class DefaultPlayerState<T>(override val name: String) : PlayerState<T> {
+    protected var dataOnEnter: T? = null
+
     override fun onEnterState(machine: PlayerStateMachine, data: T?) {
+        dataOnEnter = data
     }
 
     override fun onExitState(machine: PlayerStateMachine, elapsedTime: Long, durationInState: Long, destinationPlayerState: PlayerState<*>) {
