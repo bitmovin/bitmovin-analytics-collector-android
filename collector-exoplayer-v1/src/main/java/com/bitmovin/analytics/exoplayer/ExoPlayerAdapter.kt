09ce7f02fade69558c2ba01885c7ead5078e86f9
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2022-07-05 14:59:40 +0200

    use statemachine method instead of Thread.sleep to add artificial startuptime

--- collector-exoplayer-v1/src/main/java/com/bitmovin/analytics/exoplayer/ExoPlayerAdapter.kt
+++ collector-exoplayer-v1/src/main/java/com/bitmovin/analytics/exoplayer/ExoPlayerAdapter.kt
@@ -126,8 +126,8 @@ class ExoPlayerAdapter(
                     "Collector was attached while media source was already playing, transitioning to playing state"
                 )
 
-                // We need to wait just a little to not run into videoStartupTime = 0
-                Thread.sleep(1)
+                // We need to add at least one ms here to not run into videoStartupTime = 0
+                stateMachine.addStartupTime(1)
                 stateMachine.transitionState(PlayerStates.PLAYING, position)
             }
         }
