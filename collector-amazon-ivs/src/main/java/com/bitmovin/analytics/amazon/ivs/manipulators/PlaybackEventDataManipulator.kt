7ae9ed9799ab7f3049d962d2522175f66d9d5d02
petereder <104372727+petereder@users.noreply.github.com>
2023-03-06 18:46:11 +0100

    Remove automatic detecting of videoId (#203)

--- collector-amazon-ivs/src/main/java/com/bitmovin/analytics/amazon/ivs/manipulators/PlaybackEventDataManipulator.kt
+++ collector-amazon-ivs/src/main/java/com/bitmovin/analytics/amazon/ivs/manipulators/PlaybackEventDataManipulator.kt
@@ -20,42 +20,10 @@ internal class PlaybackEventDataManipulator(
             // IVS player only supports HLS, thus we hardcode it here
             data.streamFormat = StreamFormat.HLS.value
 
-            setVideoId(data)
             setLive(data)
         } catch (e: Exception) {
             Log.e("PlaybackDataManipulator", "Something went wrong while setting playback event data, e: ${e.message}", e)
         }
-    }
-
-    // applies the channelId and sessionId if config.videoId is not set
-    private fun setVideoId(data: EventData) {
-        if (config.videoId != null) {
-            return
-        }
-
-        val sessionId = player.sessionId
-        val channelId = getChannelIdFromUrl(config.m3u8Url)
-        if (sessionId.isBlank() || channelId == null || channelId.isBlank()) {
-            return
-        }
-
-        data.videoId = "$channelId-$sessionId"
-    }
-    private fun getChannelIdFromUrl(url: String?): String? {
-        url ?: return null
-
-        val indexOfChannelPrefix = url.indexOf(CHANNEL_PREFIX)
-        if (indexOfChannelPrefix == -1) {
-            return null
-        }
-
-        val indexOfExtension = url.indexOf(HLS_EXTENSION)
-        if (indexOfExtension == -1) {
-            return null
-        }
-
-        val indexOfChannelId = indexOfChannelPrefix + CHANNEL_PREFIX.length
-        return url.substring(indexOfChannelId until indexOfExtension)
     }
 
     private fun setLive(data: EventData) {
