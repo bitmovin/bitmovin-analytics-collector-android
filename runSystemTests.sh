c28ca14f2476a5bba3661fcc31d28f8924d9e6f7
Peter Eder <peter.eder@bitmovin.com>
2023-03-23 11:28:42 +0100

    Add auto accepting of licenses to systemtest

--- runSystemTests.sh
+++ runSystemTests.sh
@@ -1,1 +1,5 @@
-./gradlew cleanPixel6api30DebugAndroidTest collector-amazon-ivs-example:connectedAndroidTest pixel6api30DebugAndroidTest
+## accept all sdk licenses
+yes | /Users/$USER/Library/Android/sdk/cmdline-tools/latest/bin/sdkmanager --licenses
+
+## run systemtest for ivs
+./gradlew cleanPixel6api30DebugAndroidTest :collector-amazon-ivs-example:connectedAndroidTest pixel6api30DebugAndroidTest
