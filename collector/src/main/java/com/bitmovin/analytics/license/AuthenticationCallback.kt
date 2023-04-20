61e6afc70ac394d5fcc1768cac039b067215ec4e
Matthias Tamegger <matamegger@gmail.com>
2023-04-20 11:00:05 +0200

    Introduce AuthenticationResponse

--- collector/src/main/java/com/bitmovin/analytics/license/AuthenticationCallback.kt
+++ collector/src/main/java/com/bitmovin/analytics/license/AuthenticationCallback.kt
@@ -3,3 +3,15 @@ package com.bitmovin.analytics.license
 interface AuthenticationCallback {
     fun authenticationCompleted(success: Boolean, featureConfigs: FeatureConfigContainer?)
 }
+
+sealed class AuthenticationResponse {
+    data class Granted(
+        val featureConfigContainer: FeatureConfigContainer?
+    ): AuthenticationResponse()
+
+    data class Denied(
+        val message: String
+    ): AuthenticationResponse()
+
+    object Error : AuthenticationResponse()
+}
