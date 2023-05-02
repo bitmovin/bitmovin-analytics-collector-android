3349f9123d92c75f212bcc15019824f8f52f81e7
Lucas <lucas.gruenberger@bitmovin.com>
2023-05-02 08:34:44 +0200

    Make use of kotlin-parcelize
    [PA-1959]

--- collector/src/main/java/com/bitmovin/analytics/api/AnalyticsConfig.kt
+++ collector/src/main/java/com/bitmovin/analytics/api/AnalyticsConfig.kt
@@ -1,9 +1,10 @@
 package com.bitmovin.analytics.api
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.bitmovin.analytics.CollectorConfig
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class AnalyticsConfig
 @JvmOverloads
 constructor(
@@ -192,103 +193,4 @@ constructor(
      * Specifies if failed requests should be resent again.
      */
     val tryResendDataOnFailedConnection: Boolean = false
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        // TODO: should we throw, if there is no key in the bundle?
-        key = parcel.readString() ?: "",
-        cdnProvider = parcel.readString(),
-        customData1 = parcel.readString(),
-        customData2 = parcel.readString(),
-        customData3 = parcel.readString(),
-        customData4 = parcel.readString(),
-        customData5 = parcel.readString(),
-        customData6 = parcel.readString(),
-        customData7 = parcel.readString(),
-        customData8 = parcel.readString(),
-        customData9 = parcel.readString(),
-        customData10 = parcel.readString(),
-        customData11 = parcel.readString(),
-        customData12 = parcel.readString(),
-        customData13 = parcel.readString(),
-        customData14 = parcel.readString(),
-        customData15 = parcel.readString(),
-        customData16 = parcel.readString(),
-        customData17 = parcel.readString(),
-        customData18 = parcel.readString(),
-        customData19 = parcel.readString(),
-        customData20 = parcel.readString(),
-        customData21 = parcel.readString(),
-        customData22 = parcel.readString(),
-        customData23 = parcel.readString(),
-        customData24 = parcel.readString(),
-        customData25 = parcel.readString(),
-        customData26 = parcel.readString(),
-        customData27 = parcel.readString(),
-        customData28 = parcel.readString(),
-        customData29 = parcel.readString(),
-        customData30 = parcel.readString(),
-        customUserId = parcel.readString(),
-        experimentName = parcel.readString(),
-        ads = parcel.readByte() != 0.toByte(),
-        randomizeUserId = parcel.readByte() != 0.toByte(),
-        // TODO: should we throw, if there is no backendUrl in the bundle?
-        backendUrl = parcel.readString() ?: "",
-        tryResendDataOnFailedConnection = parcel.readByte() != 0.toByte()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(key)
-        parcel.writeString(cdnProvider)
-        parcel.writeString(customData1)
-        parcel.writeString(customData2)
-        parcel.writeString(customData3)
-        parcel.writeString(customData4)
-        parcel.writeString(customData5)
-        parcel.writeString(customData6)
-        parcel.writeString(customData7)
-        parcel.writeString(customData8)
-        parcel.writeString(customData9)
-        parcel.writeString(customData10)
-        parcel.writeString(customData11)
-        parcel.writeString(customData12)
-        parcel.writeString(customData13)
-        parcel.writeString(customData14)
-        parcel.writeString(customData15)
-        parcel.writeString(customData16)
-        parcel.writeString(customData17)
-        parcel.writeString(customData18)
-        parcel.writeString(customData19)
-        parcel.writeString(customData20)
-        parcel.writeString(customData21)
-        parcel.writeString(customData22)
-        parcel.writeString(customData23)
-        parcel.writeString(customData24)
-        parcel.writeString(customData25)
-        parcel.writeString(customData26)
-        parcel.writeString(customData27)
-        parcel.writeString(customData28)
-        parcel.writeString(customData29)
-        parcel.writeString(customData30)
-        parcel.writeString(customUserId)
-        parcel.writeString(experimentName)
-        parcel.writeByte(if (ads) 1 else 0)
-        parcel.writeByte(if (randomizeUserId) 1 else 0)
-        parcel.writeString(backendUrl)
-        parcel.writeByte(if (tryResendDataOnFailedConnection) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<AnalyticsConfig> {
-        override fun createFromParcel(parcel: Parcel): AnalyticsConfig {
-            return AnalyticsConfig(parcel)
-        }
-
-        override fun newArray(size: Int): Array<AnalyticsConfig?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
