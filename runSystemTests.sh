400e4c6639a483fca0f02a567a5abe11fdc957c5
petereder <104372727+petereder@users.noreply.github.com>
2023-05-04 18:43:27 +0200

    Stabilize flaky ivs systemtest (#255)

--- runSystemTests.sh
+++ runSystemTests.sh
@@ -18,3 +18,9 @@ yes | /Users/$USER/Library/Android/sdk/cmdline-tools/latest/bin/sdkmanager --lic
 ./gradlew :collector-bitmovin-player:cleanPixel6api30DebugAndroidTest
 ## run test
 ./gradlew :collector-bitmovin-player:pixel6api30DebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=com.bitmovin.analytics.bitmovin.player.PhoneBasicScenariosTest || exit
+
+# command to run specific test (with regex) in a loop (can be used to verify flaky test is stable)
+#for i in {1..10}; do
+#  ./gradlew :collector-amazon-ivs:cleanPixel6api30DebugAndroidTest
+#  ./gradlew :collector-amazon-ivs:pixel6api30DebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.tests_regex=com.bitmovin.analytics.amazon.ivs.PhoneBasicScenariosTest.test_nonExistingStream_Should_sendErrorSample || exit
+#done
