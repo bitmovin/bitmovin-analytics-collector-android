04578e229a61da31dca3a2f565c718710ee13c69
Matthias Tamegger <matamegger@gmail.com>
2023-05-04 14:39:32 +0200

    Use actual adevent time

--- collector/src/test/java/com/bitmovin/analytics/data/persistence/PersistentAnalyticsEventQueueTest.kt
+++ collector/src/test/java/com/bitmovin/analytics/data/persistence/PersistentAnalyticsEventQueueTest.kt
@@ -51,7 +51,7 @@ class PersistentAnalyticsEventQueueTest {
     fun `pushing an AdEventData pushes an according AdEventDatabaseEntry to the event database`() {
         val event = TestFactory.createAdEventData()
         val eventDatabaseEntry = EventDatabaseEntry(
-            -1,
+            event.time,
             DataSerializer.serialize(event)!!,
         )
 
@@ -90,7 +90,7 @@ class PersistentAnalyticsEventQueueTest {
     fun `popping an AdEventData pops from the event database`() {
         val event = TestFactory.createAdEventData()
         val eventDatabaseEntry = EventDatabaseEntry(
-            -1, // TODO: time in AdEventData is nullable. Discuss this.
+            event.time,
             DataSerializer.serialize(event)!!,
         )
         every { adEventDatabase.pop() } returns eventDatabaseEntry
