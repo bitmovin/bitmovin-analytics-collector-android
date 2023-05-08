d21d52fce39fa4a03c87921058ce3a2164862f91
Matthias Tamegger <matamegger@gmail.com>
2023-05-08 14:33:13 +0200

    Remove locking mechanism from the InMemoryQueue

--- collector/src/test/java/com/bitmovin/analytics/testutils/InMemoryQueue.kt
+++ collector/src/test/java/com/bitmovin/analytics/testutils/InMemoryQueue.kt
@@ -1,31 +1,17 @@
 package com.bitmovin.analytics.testutils
 
 import com.bitmovin.analytics.persistence.queue.EventQueue
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import java.util.LinkedList
 import java.util.Queue
 
 internal class InMemoryQueue<T> : EventQueue<T> {
     private val queue: Queue<T> = LinkedList()
-    private val mutex: Mutex = Mutex()
 
-    override fun push(event: T): Unit = runBlocking {
-        mutex.withLock {
-            queue.offer(event)
-        }
+    override fun push(event: T) {
+        queue.offer(event)
     }
 
-    override fun pop() = runBlocking {
-        mutex.withLock {
-            queue.poll()
-        }
-    }
+    override fun pop() = queue.poll()
 
-    override fun clear() = runBlocking {
-        mutex.withLock {
-            queue.clear()
-        }
-    }
+    override fun clear() = queue.clear()
 }
