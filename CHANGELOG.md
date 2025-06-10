c62972cf95956e9593089c46534b6eea2527b6b1
petereder <104372727+petereder@users.noreply.github.com>
2025-06-10 10:13:13 +0200

    Reuse Json instance for DTO serialization (#487)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 ## Development
 
+### Fixed
+- [Internal] Reuse Json Instance for serialization of DTOs for better performance
+
 ### v3.15.0
 
 ### Changed
--- collector/src/main/java/com/bitmovin/analytics/utils/DataSerializerKotlinX.kt
+++ collector/src/main/java/com/bitmovin/analytics/utils/DataSerializerKotlinX.kt
@@ -9,45 +9,43 @@ object DataSerializerKotlinX {
     // We use an experimental API here (explicitNulls flag),
     // but this one was promoted to stable with kotlinx.serialization 1.7.0
     // so it is safe to use
+    // We are reusing the instance as recommended in the documentation:
+    // https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/json.md#json-configuration
+
+    // for encoding
+    // encodeDefaults = true is needed to ensure that all properties (also the ones with defaults)
+    //      are serialized, this mimics the behavior of Gson which we had before
+    // explicitNulls = false makes sure that null values are not serialized and thus reduces request size
+
+    // for decoding
+    // encodeDefaults = true is needed to ensure that all properties (also the ones with defaults)
+    //      are serialized, this mimics the behavior of Gson which we had before
+    // explicitNulls = false is needed to avoid setting of default values
+    //      for all nullable properties in the data classes
+    // ignoreUnknownKeys = true this is needed for backward compatibility,
+    //      in case we introduce new properties in the data classes
     @OptIn(ExperimentalSerializationApi::class)
+    val jsonInstance: Json =
+        Json {
+            encodeDefaults = true
+            explicitNulls = false
+            ignoreUnknownKeys = true
+        }
+
     inline fun <reified T> serialize(data: T?): String? {
         return data?.let {
-            // encodeDefaults = true is needed to ensure that all properties (also the ones with defaults)
-            //      are serialized, this mimics the behavior of Gson which we had before
-            // explicitNulls = false makes sure that null values are not serialized and thus reduces request size
-            val json =
-                Json {
-                    encodeDefaults = true
-                    explicitNulls = false
-                }
-            json.encodeToString(it)
+            jsonInstance.encodeToString(it)
         }
     }
 
-    // We use an experimental API here (explicitNulls flag),
-    // but this one was promoted to stable with kotlinx.serialization 1.7.0
-    // so it is safe to use
     // clazz is used as parameter to ease inferring of type
-    @OptIn(ExperimentalSerializationApi::class)
     inline fun <reified T> deserialize(
         input: String?,
         clazz: Class<T>,
     ): T? {
         return input?.let {
             return try {
-                // encodeDefaults = true is needed to ensure that all properties (also the ones with defaults)
-                //      are serialized, this mimics the behavior of Gson which we had before
-                // explicitNulls = false is needed to avoid setting of default values
-                //      for all nullable properties in the data classes
-                // ignoreUnknownKeys = true this is needed for backward compatibility,
-                //      in case we introduce new properties in the data classes
-                val json =
-                    Json {
-                        encodeDefaults = true
-                        explicitNulls = false
-                        ignoreUnknownKeys = true
-                    }
-                json.decodeFromString<T>(input)
+                jsonInstance.decodeFromString<T>(input)
             } catch (e: SerializationException) {
                 BitmovinLog.e("DataSerializerKotlinX", "Failed to deserialize data for class: ${clazz.simpleName} : $e")
                 null
